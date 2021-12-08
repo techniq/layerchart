fd7f42d2e23dddf7f4e05b251fcd9764b1da34e6
Sean Lynch <techniq35@gmail.com>
2021-12-08 07:51:53 -0500

    [Points] Support multiple per axis (ex. x={"prop1", "prop2"]).  Pass context to function offsets for access to scales

--- src/lib/components/Points.svelte
+++ src/lib/components/Points.svelte
@@ -1,28 +1,63 @@
-<script>
+<script lang="ts">
 	import { getContext } from 'svelte';
 
 	import Circle from './Circle.svelte';
 
-	const { data, xGet, yGet, xScale, yScale } = getContext('LayerCake');
+	const context = getContext('LayerCake') as any;
+	const { data, xGet, yGet, config } = context;
+
+	type Offset = number | ((value: number, context: any) => number);
 
 	export let r = 5;
-	export let fill = '#000';
-	export let stroke = '#000';
-	export let strokeWidth = 0;
-	export let dx = 0;
-	export let dy = 0;
+	export let offsetX: Offset = 0;
+	export let offsetY: Offset = 0;
+
+	function getOffset(value, offset: Offset) {
+		if (typeof offset === 'function') {
+			return offset(value, context);
+		} else {
+			return offset;
+		}
+	}
+
+	$: points = $data.flatMap((d) => {
+		if (Array.isArray($config.x)) {
+			/*
+				x={["prop1" ,"prop2"]}
+				y="prop3"
+			*/
+			return $xGet(d).map((x) => {
+				return {
+					x: x + getOffset(x, offsetX),
+					y: $yGet(d) + getOffset($yGet(d), offsetY)
+				};
+			});
+		} else if (Array.isArray($config.y)) {
+			/*
+				x="prop1"
+				y={["prop2" ,"prop3"]}
+			*/
+			return $yGet(d).map((y) => {
+				return {
+					x: $xGet(d) + getOffset($xGet(d), offsetX),
+					y: y + getOffset(y, offsetY)
+				};
+			});
+		} else {
+			/*
+				x="prop1"
+				y="prop2"
+			*/
+			return {
+				x: $xGet(d) + getOffset($xGet(d), offsetX),
+				y: $yGet(d) + getOffset($yGet(d), offsetY)
+			};
+		}
+	});
 </script>
 
-<g class="scatter-group">
-	{#each $data as d}
-		<Circle
-			cx={$xGet(d) + (typeof dx === 'function' ? dx($xScale) : dx)}
-			cy={$yGet(d) + (typeof dy === 'function' ? dy($yScale) : dy)}
-			{r}
-			{fill}
-			{stroke}
-			stroke-width={strokeWidth}
-			{...$$restProps}
-		/>
+<g class="point-group">
+	{#each points as p}
+		<Circle cx={p.x} cy={p.y} {r} {...$$restProps} />
 	{/each}
 </g>
