1c9c38ffc31605b74a508ee26ea52a26d018890b
Sean Lynch <techniq35@gmail.com>
2021-12-13 19:18:07 -0500

    Add Group component for easy placement / center

---
+++ src/lib/components/Group.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	/**
+	 * Translate x
+	 */
+	export let x: number;
+
+	/**
+	 * Translate x
+	 */
+	export let y: number;
+
+	/**
+	 * Center within chart
+	 */
+	export let center: boolean;
+
+	const { width, height } = getContext('LayerCake');
+
+	let transform = undefined;
+
+	$: if (x != null || y != null) {
+		transform = `translate(${x ?? 0}, ${y ?? 0})`;
+	}
+
+	$: if (center) {
+		transform = `translate(${$width / 2}, ${$height / 2})`;
+	}
+</script>
+
+<g {transform} {...$$restProps}>
+	<slot />
+</g>
