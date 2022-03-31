bc0a76ce221c84c645f5cdb3c8476766afc7fb7a
Sean Lynch <techniq35@gmail.com>
2022-03-31 10:58:50 -0400

    Move chart examples to top of menu

--- src/routes/_NavMenu.svelte
+++ src/routes/_NavMenu.svelte
@@ -5,6 +5,15 @@
 
 <NavItem text="Home" icon={mdiHome} path="/" class="pl-6 py-2 mt-2" />
 
+<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Chart Examples</div>
+<NavItem text="Area" path="/examples/Area" class="pl-6 py-2" />
+<NavItem text="AreaStack" path="/examples/AreaStack" class="pl-6 py-2" />
+<NavItem text="Bars" path="/examples/Bars" class="pl-6 py-2" />
+<NavItem text="ClevelandDotPlot" path="/examples/ClevelandDotPlot" class="pl-6 py-2" />
+<NavItem text="Pie" path="/examples/Pie" class="pl-6 py-2" />
+<NavItem text="Scatter" path="/examples/Scatter" class="pl-6 py-2" />
+<NavItem text="Threshold" path="/examples/Threshold" class="pl-6 py-2" />
+
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Common</div>
 <NavItem text="Chart" path="/components/Chart" class="pl-6 py-2" />
 <NavItem text="Axis" path="/components/Axis" class="pl-6 py-2" />
@@ -36,12 +45,3 @@
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Utils</div>
 <NavItem text="pivot" path="/utils/pivot" class="pl-6 py-2" />
-
-<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Chart Examples</div>
-<NavItem text="Area" path="/examples/Area" class="pl-6 py-2" />
-<NavItem text="AreaStack" path="/examples/AreaStack" class="pl-6 py-2" />
-<NavItem text="Bars" path="/examples/Bars" class="pl-6 py-2" />
-<NavItem text="ClevelandDotPlot" path="/examples/ClevelandDotPlot" class="pl-6 py-2" />
-<NavItem text="Pie" path="/examples/Pie" class="pl-6 py-2" />
-<NavItem text="Scatter" path="/examples/Scatter" class="pl-6 py-2" />
-<NavItem text="Threshold" path="/examples/Threshold" class="pl-6 py-2" />
