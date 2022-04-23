f0025555f983d52b32a788f37d1784a31136d04e
Sean Lynch <techniq35@gmail.com>
2022-04-23 01:12:41 -0400

    Add more example graph data

--- src/routes/examples/data/graph.ts
+++ src/routes/examples/data/graph.ts
@@ -1,3 +1,8 @@
+/*
+	TODO:
+	  - https://observablehq.com/@sophietalbot/bipartite-graph-of-virtual-water-trade-wheat/2
+*/
+
 export const simpleData = {
 	nodes: [
 		{ id: 'A1' },
@@ -150,5 +155,308 @@ export const complexData = {
 		{ source: 45, target: 1, value: 182.01 },
 		{ source: 46, target: 15, value: 19.013 },
 		{ source: 47, target: 15, value: 289.366 }
+	]
+};
+
+// https://www.fusioncharts.com/charts/sankey-diagram/vertical-sankey-diagram?framework=javascript
+export const bilateralTradeValue2013 = {
+	nodes: [
+		{
+			label: 'Netherlands'
+		},
+		{
+			label: 'Canada'
+		},
+		{
+			label: 'Belgium'
+		},
+		{
+			label: 'Italy'
+		},
+		{
+			label: 'Mexico'
+		},
+		{
+			label: 'Russia'
+		},
+		{
+			label: 'Spain'
+		},
+		{
+			label: 'South Korea'
+		},
+		{
+			label: 'Germany'
+		},
+		{
+			label: 'China'
+		},
+		{
+			label: 'European Union'
+		},
+		{
+			label: 'Japan'
+		},
+		{
+			label: 'United Kingdom'
+		},
+		{
+			label: 'United States'
+		},
+		{
+			label: 'France'
+		},
+		{
+			label: 'Hong Kong'
+		},
+		{
+			label: 'Switzerland'
+		},
+		{
+			label: 'Austria'
+		},
+		{
+			label: 'Sweden'
+		}
+	],
+	links: [
+		{
+			from: 'Netherlands',
+			to: 'European Union',
+			value: 798744
+		},
+		{
+			from: 'Germany',
+			to: 'European Union',
+			value: 1468990
+		},
+		{
+			from: 'European Union',
+			to: 'France',
+			value: 745931
+		},
+		{
+			from: 'European Union',
+			to: 'United States',
+			value: 660541
+		},
+		{
+			from: 'Canada',
+			to: 'United States',
+			value: 594546
+		},
+		{
+			from: 'Belgium',
+			to: 'European Union',
+			value: 628796
+		},
+		{
+			from: 'China',
+			to: 'Hong Kong',
+			value: 400571
+		},
+		{
+			from: 'China',
+			to: 'United States',
+			value: 526454
+		},
+		{
+			from: 'European Union',
+			to: 'United Kingdom',
+			value: 520318
+		},
+		{
+			from: 'China',
+			to: 'European Union',
+			value: 560536
+		},
+		{
+			from: 'Italy',
+			to: 'European Union',
+			value: 539556
+		},
+		{
+			from: 'Mexico',
+			to: 'United States',
+			value: 492715
+		},
+		{
+			from: 'Russia',
+			to: 'European Union',
+			value: 385778
+		},
+		{
+			from: 'Spain',
+			to: 'European Union',
+			value: 365191
+		},
+		{
+			from: 'China',
+			to: 'Japan',
+			value: 312062
+		},
+		{
+			from: 'European Union',
+			to: 'Switzerland',
+			value: 328609
+		},
+		{
+			from: 'South Korea',
+			to: 'China',
+			value: 229073
+		},
+		{
+			from: 'European Union',
+			to: 'Austria',
+			value: 244913
+		},
+		{
+			from: 'Japan',
+			to: 'United States',
+			value: 206091
+		},
+		{
+			from: 'European Union',
+			to: 'Sweden',
+			value: 204849
+		},
+		{
+			from: 'Germany',
+			to: 'United States',
+			value: 184287
+		}
+	]
+};
+
+// https://bl.ocks.org/d3noob/5028304
+export const greenhouse = {
+	links: [
+		{ source: 'Agricultural Energy Use', target: 'Carbon Dioxide', value: '1.4' },
+		{ source: 'Agriculture', target: 'Agriculture Soils', value: '5.2' },
+		{ source: 'Agriculture', target: 'Livestock and Manure', value: '5.4' },
+		{ source: 'Agriculture', target: 'Other Agriculture', value: '1.7' },
+		{ source: 'Agriculture', target: 'Rice Cultivation', value: '1.5' },
+		{ source: 'Agriculture Soils', target: 'Nitrous Oxide', value: '5.2' },
+		{ source: 'Air', target: 'Carbon Dioxide', value: '1.7' },
+		{ source: 'Aluminium Non-Ferrous Metals', target: 'Carbon Dioxide', value: '1.0' },
+		{ source: 'Aluminium Non-Ferrous Metals', target: 'HFCs - PFCs', value: '0.2' },
+		{ source: 'Cement', target: 'Carbon Dioxide', value: '5.0' },
+		{ source: 'Chemicals', target: 'Carbon Dioxide', value: '3.4' },
+		{ source: 'Chemicals', target: 'HFCs - PFCs', value: '0.5' },
+		{ source: 'Chemicals', target: 'Nitrous Oxide', value: '0.2' },
+		{ source: 'Coal Mining', target: 'Carbon Dioxide', value: '0.1' },
+		{ source: 'Coal Mining', target: 'Methane', value: '1.2' },
+		{ source: 'Commercial Buildings', target: 'Carbon Dioxide', value: '6.3' },
+		{ source: 'Deforestation', target: 'Carbon Dioxide', value: '10.9' },
+		{ source: 'Electricity and heat', target: 'Agricultural Energy Use', value: '0.4' },
+		{ source: 'Electricity and heat', target: 'Aluminium Non-Ferrous Metals', value: '0.4' },
+		{ source: 'Electricity and heat', target: 'Cement', value: '0.3' },
+		{ source: 'Electricity and heat', target: 'Chemicals', value: '1.3' },
+		{ source: 'Electricity and heat', target: 'Commercial Buildings', value: '5.0' },
+		{ source: 'Electricity and heat', target: 'Food and Tobacco', value: '0.5' },
+		{ source: 'Electricity and heat', target: 'Iron and Steel', value: '1.0' },
+		{ source: 'Electricity and heat', target: 'Machinery', value: '1.0' },
+		{ source: 'Electricity and heat', target: 'Oil and Gas Processing', value: '0.4' },
+		{ source: 'Electricity and heat', target: 'Other Industry', value: '2.7' },
+		{ source: 'Electricity and heat', target: 'Pulp - Paper and Printing', value: '0.6' },
+		{ source: 'Electricity and heat', target: 'Residential Buildings', value: '5.2' },
+		{ source: 'Electricity and heat', target: 'T and D Losses', value: '2.2' },
+		{ source: 'Electricity and heat', target: 'Unallocated Fuel Combustion', value: '2.0' },
+		{ source: 'Energy', target: 'Electricity and heat', value: '24.9' },
+		{ source: 'Energy', target: 'Fugitive Emissions', value: '4.0' },
+		{ source: 'Energy', target: 'Industry', value: '14.7' },
+		{ source: 'Energy', target: 'Other Fuel Combustion', value: '8.6' },
+		{ source: 'Energy', target: 'Transportation', value: '14.3' },
+		{ source: 'Food and Tobacco', target: 'Carbon Dioxide', value: '1.0' },
+		{ source: 'Fugitive Emissions', target: 'Coal Mining', value: '1.3' },
+		{ source: 'Fugitive Emissions', target: 'Oil and Gas Processing', value: '3.2' },
+		{ source: 'Harvest / Management', target: 'Carbon Dioxide', value: '1.3' },
+		{ source: 'Industrial Processes', target: 'Aluminium Non-Ferrous Metals', value: '0.4' },
+		{ source: 'Industrial Processes', target: 'Cement', value: '2.8' },
+		{ source: 'Industrial Processes', target: 'Chemicals', value: '1.4' },
+		{ source: 'Industrial Processes', target: 'Other Industry', value: '0.5' },
+		{ source: 'Industry', target: 'Aluminium Non-Ferrous Metals', value: '0.4' },
+		{ source: 'Industry', target: 'Cement', value: '1.9' },
+		{ source: 'Industry', target: 'Chemicals', value: '1.4' },
+		{ source: 'Industry', target: 'Food and Tobacco', value: '0.5' },
+		{ source: 'Industry', target: 'Iron and Steel', value: '3.0' },
+		{ source: 'Industry', target: 'Oil and Gas Processing', value: '2.8' },
+		{ source: 'Industry', target: 'Other Industry', value: '3.8' },
+		{ source: 'Industry', target: 'Pulp - Paper and Printing', value: '0.5' },
+		{ source: 'Iron and Steel', target: 'Carbon Dioxide', value: '4.0' },
+		{ source: 'Land Use Change', target: 'Deforestation', value: '10.9' },
+		{ source: 'Land Use Change', target: 'Harvest / Management', value: '1.3' },
+		{ source: 'Landfills', target: 'Methane', value: '1.7' },
+		{ source: 'Livestock and Manure', target: 'Methane', value: '5.1' },
+		{ source: 'Livestock and Manure', target: 'Nitrous Oxide', value: '0.3' },
+		{ source: 'Machinery', target: 'Carbon Dioxide', value: '1.0' },
+		{ source: 'Oil and Gas Processing', target: 'Carbon Dioxide', value: '3.6' },
+		{ source: 'Oil and Gas Processing', target: 'Methane', value: '2.8' },
+		{ source: 'Other Agriculture', target: 'Methane', value: '1.4' },
+		{ source: 'Other Agriculture', target: 'Nitrous Oxide', value: '0.3' },
+		{ source: 'Other Fuel Combustion', target: 'Agricultural Energy Use', value: '1.0' },
+		{ source: 'Other Fuel Combustion', target: 'Commercial Buildings', value: '1.3' },
+		{ source: 'Other Fuel Combustion', target: 'Residential Buildings', value: '5.0' },
+		{ source: 'Other Fuel Combustion', target: 'Unallocated Fuel Combustion', value: '1.8' },
+		{ source: 'Other Industry', target: 'Carbon Dioxide', value: '6.6' },
+		{ source: 'Other Industry', target: 'HFCs - PFCs', value: '0.4' },
+		{ source: 'Pulp - Paper and Printing', target: 'Carbon Dioxide', value: '1.1' },
+		{ source: 'Rail - Ship and Other Transport', target: 'Carbon Dioxide', value: '2.5' },
+		{ source: 'Residential Buildings', target: 'Carbon Dioxide', value: '10.2' },
+		{ source: 'Rice Cultivation', target: 'Methane', value: '1.5' },
+		{ source: 'Road', target: 'Carbon Dioxide', value: '10.5' },
+		{ source: 'T and D Losses', target: 'Carbon Dioxide', value: '2.2' },
+		{ source: 'Transportation', target: 'Air', value: '1.7' },
+		{ source: 'Transportation', target: 'Rail - Ship and Other Transport', value: '2.5' },
+		{ source: 'Transportation', target: 'Road', value: '10.5' },
+		{ source: 'Unallocated Fuel Combustion', target: 'Carbon Dioxide', value: '3.0' },
+		{ source: 'Unallocated Fuel Combustion', target: 'Methane', value: '0.4' },
+		{ source: 'Unallocated Fuel Combustion', target: 'Nitrous Oxide', value: '0.4' },
+		{ source: 'Waste', target: 'Landfills', value: '1.7' },
+		{ source: 'Waste', target: 'Waste water - Other Waste', value: '1.5' },
+		{ source: 'Waste water - Other Waste', target: 'Methane', value: '1.2' },
+		{ source: 'Waste water - Other Waste', target: 'Nitrous Oxide', value: '0.3' }
+	],
+	nodes: [
+		{ name: 'Energy' },
+		{ name: 'Industrial Processes' },
+		{ name: 'Electricity and heat' },
+		{ name: 'Industry' },
+		{ name: 'Land Use Change' },
+		{ name: 'Agriculture' },
+		{ name: 'Waste' },
+		{ name: 'Transportation' },
+		{ name: 'Other Fuel Combustion' },
+		{ name: 'Fugitive Emissions' },
+		{ name: 'Road' },
+		{ name: 'Air' },
+		{ name: 'Rail - Ship and Other Transport' },
+		{ name: 'Residential Buildings' },
+		{ name: 'Commercial Buildings' },
+		{ name: 'Unallocated Fuel Combustion' },
+		{ name: 'Iron and Steel' },
+		{ name: 'Aluminium Non-Ferrous Metals' },
+		{ name: 'Machinery' },
+		{ name: 'Pulp - Paper and Printing' },
+		{ name: 'Food and Tobacco' },
+		{ name: 'Chemicals' },
+		{ name: 'Cement' },
+		{ name: 'Other Industry' },
+		{ name: 'T and D Losses' },
+		{ name: 'Coal Mining' },
+		{ name: 'Oil and Gas Processing' },
+		{ name: 'Deforestation' },
+		{ name: 'Harvest / Management' },
+		{ name: 'Agricultural Energy Use' },
+		{ name: 'Agriculture Soils' },
+		{ name: 'Livestock and Manure' },
+		{ name: 'Rice Cultivation' },
+		{ name: 'Other Agriculture' },
+		{ name: 'Landfills' },
+		{ name: 'Waste water - Other Waste' },
+		{ name: 'Carbon Dioxide' },
+		{ name: 'HFCs - PFCs' },
+		{ name: 'Methane' },
+		{ name: 'Nitrous Oxide' }
 	]
 };
