4a0c5c5c0c042333d0bdd28403980f967a1d78d4
Sean Lynch <techniq35@gmail.com>
2022-07-24 23:30:38 -0400

    [Rect] Support passing per-property motion options.  Issue #21

--- src/lib/components/Rect.svelte
+++ src/lib/components/Rect.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-	import type { spring as springStore, tweened as tweenedStore } from 'svelte/motion';
-
-	import { motionStore } from '$lib/stores/motionStore';
+	import {
+		motionStore,
+		resolveOptions,
+		SpringOptions,
+		TweenedOptions
+	} from '$lib/stores/motionStore';
 
 	export let x: number = 0;
 	export let y: number = 0;
 	export let width: number;
 	export let height: number;
-	export let spring: boolean | Parameters<typeof springStore>[1] = undefined;
-	export let tweened: boolean | Parameters<typeof tweenedStore>[1] = undefined;
+	export let spring: boolean | SpringOptions | { [prop: string]: SpringOptions } = undefined;
+	export let tweened: boolean | TweenedOptions | { [prop: string]: TweenedOptions } = undefined;
 
-	let tweened_x = motionStore(x, { spring, tweened });
-	let tweened_y = motionStore(y, { spring, tweened });
-	let tweened_width = motionStore(width, { spring, tweened });
-	let tweened_height = motionStore(height, { spring, tweened });
+	let tweened_x = motionStore(x, resolveOptions('x', { spring, tweened }));
+	let tweened_y = motionStore(y, resolveOptions('y', { spring, tweened }));
+	let tweened_width = motionStore(width, resolveOptions('width', { spring, tweened }));
+	let tweened_height = motionStore(height, resolveOptions('height', { spring, tweened }));
 
 	$: tweened_x.set(x);
 	$: tweened_y.set(y);
--- src/lib/stores/motionStore.ts
+++ src/lib/stores/motionStore.ts
@@ -1,9 +1,17 @@
 import { writable } from 'svelte/store';
 import { spring, tweened } from 'svelte/motion';
 
+export type SpringOptions = Parameters<typeof spring>[1];
+export type TweenedOptions = Parameters<typeof tweened>[1];
+
 export type MotionOptions = {
-	spring?: boolean | Parameters<typeof spring>[1];
-	tweened?: boolean | Parameters<typeof tweened>[1];
+	spring?: boolean | SpringOptions;
+	tweened?: boolean | TweenedOptions;
+};
+
+export type PropMotionOptions = {
+	spring?: boolean | SpringOptions | { [prop: string]: SpringOptions };
+	tweened?: boolean | TweenedOptions | { [prop: string]: TweenedOptions };
 };
 
 /**
@@ -17,4 +25,24 @@ export function motionStore(value: any, options: MotionOptions) {
 	} else {
 		return writable(value);
 	}
+}
+
+/**
+ * Helper to resolve motion options with specific property option (useful for specifying per-prop delays)
+ */
+export function resolveOptions(prop: string, options: PropMotionOptions) {
+	return {
+		spring:
+			typeof options.spring === 'boolean' || options.spring == null
+				? options.spring
+				: prop in options.spring
+				? options.spring[prop]
+				: options.spring,
+		tweened:
+			typeof options.tweened === 'boolean' || options.tweened == null
+				? options.tweened
+				: prop in options.tweened
+				? options.tweened[prop]
+				: options.tweened
+	};
 }
