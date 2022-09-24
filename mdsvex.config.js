09571bcdf54d89a01ee5ad104818555ed8468254
Sean Lynch <techniq35@gmail.com>
2022-09-24 10:09:44 -0400

    Add rehype-slug to fix ToC links

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,8 +1,10 @@
+import slug from 'rehype-slug';
 import { codePreview } from 'svelte-ux/plugins/remark';
 
 export default {
 	extensions: ['.md', '.svx'],
 	layout: './src/lib/docs/Layout.svelte',
-	remarkPlugins: [codePreview]
+	remarkPlugins: [codePreview],
+	rehypePlugins: [slug]
 	// highlight: false
 };
