5d9475e1c38eba68a82debbf9621b311f6aea99c
Sean Lynch <techniq35@gmail.com>
2023-02-07 23:20:30 -0500

    Add ClipPathUse component to conveniently reference another path by id to use as clip-path

---
+++ src/lib/components/ClipPathUse.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { uniqueId } from 'svelte-ux/utils/string';
+
+	import ClipPath from './ClipPath.svelte';
+
+	/** Id of path or shape */
+	export let id: string;
+
+	/** Unique id for clipPath */
+	export let clipId: string = uniqueId('clipPath-');
+</script>
+
+<ClipPath id={clipId}>
+	<use href="#{id}" />
+</ClipPath>
+
+{#if $$slots.default}
+	<g style="clip-path: url(#{clipId})" on:click on:mousemove on:mouseleave>
+		<slot {id} {clipId} />
+	</g>
+{/if}
