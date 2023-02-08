37bdc356674618d62879f4c118cd2cca5d336740
Sean Lynch <techniq35@gmail.com>
2023-02-08 07:29:52 -0500

    [AnimatedGlobe] Use 1:110 scale to reduce draw time and improve animation performance

--- src/routes/docs/examples/AnimatedGlobe/+page.ts
+++ src/routes/docs/examples/AnimatedGlobe/+page.ts
@@ -4,6 +4,6 @@ export const load = (async ({ fetch }) => {
 	// TODO: Support different US (https://github.com/topojson/us-atlas) and World (https://github.com/topojson/world-atlas) files
 	// TODO: Cache: https://github.com/sveltejs/kit/issues/3642
 	return {
-		geojson: (await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json')).json()
+		geojson: (await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')).json()
 	};
 }) satisfies PageLoad;
