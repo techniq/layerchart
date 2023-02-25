f1d1e104aba044b853f214f130fb557f158496ae
Sean Lynch <techniq35@gmail.com>
2023-02-25 13:07:09 -0500

    [RangeField] Add invisible min/max values overlayed on value to fix range input resizing as value changes (causing jank)

--- src/lib/docs/RangeField.svelte
+++ src/lib/docs/RangeField.svelte
@@ -12,6 +12,10 @@
 <Field let:id {...$$restProps}>
 	<Button icon={mdiChevronLeft} on:click={() => (value -= value > min ? step : 0)} class="mr-2" />
 	<input type="range" bind:value {min} {max} {step} {id} class="h-6 w-full" />
-	<span class="ml-4 text-sm text-black/50 tabular-nums">{value}</span>
+	<span class="ml-2 text-sm text-black/50 tabular-nums text-right inline-grid">
+		<span class="col-span-full row-span-full invisible">{min}</span>
+		<span class="col-span-full row-span-full">{value}</span>
+		<span class="col-span-full row-span-full invisible">{max}</span>
+	</span>
 	<Button icon={mdiChevronRight} on:click={() => (value += value < max ? step : 0)} class="ml-2" />
 </Field>
