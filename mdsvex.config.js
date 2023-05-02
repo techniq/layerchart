7e8074972a7e67721d684237fa4dc1d3925c3151
Sean Lynch <techniq35@gmail.com>
2023-05-01 20:07:16 -0400

    Replace codePreview remark plugin with svelte preprocessor

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,8 @@
 import slug from 'rehype-slug';
-import { codePreview } from 'svelte-ux/plugins/remark.js';
 
 export default {
 	extensions: ['.md', '.svx'],
 	layout: './src/lib/docs/Layout.svelte',
-	remarkPlugins: [codePreview],
 	rehypePlugins: [slug]
 	// highlight: false
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import vercelAdapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import { codePreview } from 'svelte-ux/plugins/svelte.js';
 
 import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [mdsvex(mdsvexConfig), sveltePreprocess()],
+	preprocess: [mdsvex(mdsvexConfig), sveltePreprocess(), codePreview()],
 
 	kit: {
 		adapter: vercelAdapter()
