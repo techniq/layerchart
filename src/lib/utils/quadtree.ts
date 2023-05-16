56a9bec50c8e5d652b54e762f8ce1ccade7a51a9
Sean Lynch <techniq35@gmail.com>
2023-05-15 21:59:39 -0400

    Add types to quadtreeRects()

--- src/lib/utils/quadtree.ts
+++ src/lib/utils/quadtree.ts
@@ -1,8 +1,13 @@
+import type { Quadtree } from 'd3-quadtree';
+
+type Rect = { x: number; y: number; width: number; height: number };
+
 /**
- * Transverse guadtree and generate rect dimensions
+ * Transverse quadtree and generate rect dimensions
  */
-export function quadtreeRects(quadtree, showLeaves = true) {
-	const rects = [];
+export function quadtreeRects<T>(quadtree: Quadtree<T>, showLeaves = true) {
+	const rects: Rect[] = [];
+
 	quadtree.visit((node, x0, y0, x1, y1) => {
 		if (showLeaves || Array.isArray(node)) {
 			rects.push({ x: x0, y: y0, width: x1 - x0, height: y1 - y0 });
