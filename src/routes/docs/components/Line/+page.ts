9a653737ad379721d4dc096b90828836c368e8e3
Sean Lynch <techniq35@gmail.com>
2023-05-23 12:17:42 -0400

    Improve primative docs

---
+++ src/routes/docs/components/Line/+page.ts
@@ -0,0 +1,7 @@
+export async function load() {
+	return {
+		meta: {
+			description: '`<line>` with tweened `x`, `y`, `width` and `height` using `motionStore`'
+		}
+	};
+}
---
+++ src/routes/docs/components/Rect/+page.ts
@@ -0,0 +1,7 @@
+export async function load() {
+	return {
+		meta: {
+			description: '`<rect>` with tweened `x`, `y`, `width` and `height` using `motionStore`'
+		}
+	};
+}
