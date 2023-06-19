c9b059e244e5c68e0ddb55e0f0fbbd878523685d
Sean Lynch <techniq35@gmail.com>
2023-06-18 23:52:20 -0400

    Extract `createDimensionGetter()` derived store from Bars to allow reuse (Labels, etc)

--- src/lib/components/Bars.svelte
+++ src/lib/components/Bars.svelte
@@ -1,25 +1,11 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 	import type { spring as springStore, tweened as tweenedStore } from 'svelte/motion';
-	import { max, min } from 'd3-array';
 
 	import Rect from './Rect.svelte';
-	import { groupScaleBand, isScaleBand } from '$lib/utils/scales';
+	import { createDimensionGetter } from '$lib/utils/rect';
 
-	const {
-		data,
-		flatData,
-		xGet,
-		yGet,
-		xRange,
-		yRange,
-		xScale,
-		yScale,
-		x: xContext,
-		y: yContext,
-		rGet,
-		config
-	} = getContext('LayerCake');
+	const { data, xScale, x: xContext, y: yContext, rGet, config } = getContext('LayerCake');
 
 	/**
 	 * Override `x` from context.  Useful for multiple Bar instances
@@ -33,11 +19,6 @@
 	 */
 	export let y = $yContext;
 	$: _y = y ? (typeof y === 'string' ? (d) => d[y] : y) : $yContext;
-
-	/*
-    TODO:
-		  - [ ] Support vertical/horizontal layout (Bar/Column)
-  */
 
 	export let color: string | ((obj: { value: any; item: any; index: number }) => string) =
 		'var(--color-blue-500)';
@@ -61,105 +42,16 @@
 	export let groupPaddingInner = 0.2;
 	export let groupPaddingOuter = 0;
 
-	$: x1Scale =
-		isScaleBand($xScale) && groupBy
-			? groupScaleBand($xScale, $flatData, groupBy, {
-					inner: groupPaddingInner,
-					outer: groupPaddingOuter
-			  })
-			: null;
-
-	$: y1Scale =
-		isScaleBand($yScale) && groupBy
-			? groupScaleBand($yScale, $flatData, groupBy, {
-					inner: groupPaddingInner,
-					outer: groupPaddingOuter
-			  })
-			: null;
-
-	// TODO: Simplify and reuse in Labels, etc
-	$: getDimensions = (item) => {
-		if (isScaleBand($xScale)) {
-			return getDimensionsVertical(item);
-		} else if (isScaleBand($yScale)) {
-			return getDimensionsHorizontal(item);
-		} else {
-			// TODO: Handle both $xScale.bandwidth and $yScale.bandwidth?
-			// TODO: Handle neither $xScale.bandwidth or $yScale.bandwidth? `Math.max(0, $xGet(d)[1] - $xGet(d)[0])` instead of `$xScale.bandwidth()`
+	$: getDimensions = createDimensionGetter(getContext('LayerCake'), {
+		x,
+		y,
+		groupBy,
+		padding,
+		groupPadding: {
+			inner: groupPaddingInner,
+			outer: groupPaddingOuter
 		}
-	};
-
-	$: getDimensionsVertical = (item) => {
-		// console.log({ item, y: $y(item) });
-
-		const x = $xGet(item) + (x1Scale ? x1Scale(item[groupBy]) : 0) + padding / 2;
-		const width = Math.max(0, (x1Scale ? x1Scale.bandwidth() : $xScale.bandwidth()) - padding);
-
-		const yValue = _y(item);
-
-		let top = 0;
-		let bottom = 0;
-		if (Array.isArray(yValue)) {
-			// Array contains both top and bottom values (stack, etc);
-			top = max(yValue);
-			bottom = min(yValue);
-		} else if (yValue == null) {
-			// null/undefined value
-			top = 0;
-			bottom = 0;
-		} else if (yValue > 0) {
-			// Positive value
-			top = yValue;
-			bottom = min($yRange); // or `0`?
-		} else {
-			// Negative value
-			top = min($yRange); // or `0`?
-			bottom = yValue;
-		}
-
-		return {
-			x,
-			y: $yScale(top),
-			width,
-			height: $yScale(bottom) - $yScale(top)
-		};
-	};
-
-	$: getDimensionsHorizontal = (item) => {
-		// console.log({ item, y: $y(item) });
-
-		const y = $yGet(item) + (y1Scale ? y1Scale(item[groupBy]) : 0) + padding / 2;
-		const height = Math.max(0, (y1Scale ? y1Scale.bandwidth() : $yScale.bandwidth()) - padding);
-
-		const xValue = _x(item);
-
-		let left = 0;
-		let right = 0;
-		if (Array.isArray(xValue)) {
-			// Array contains both top and bottom values (stack, etc);
-			left = min(xValue);
-			right = max(xValue);
-		} else if (xValue == null) {
-			// null/undefined value
-			left = 0;
-			right = 0;
-		} else if (xValue > 0) {
-			// Positive value
-			left = min($xRange); // or `0`?
-			right = xValue;
-		} else {
-			// Negative value
-			left = xValue;
-			right = min($xRange); // or `0`?
-		}
-
-		return {
-			x: $xScale(left),
-			y,
-			width: $xScale(right) - $xScale(left),
-			height
-		};
-	};
+	});
 
 	function getColor(item: any, index: number) {
 		if (typeof color === 'function') {
@@ -183,7 +75,7 @@
 			rx={radius}
 			{spring}
 			{tweened}
-			{...getDimensions(item)}
+			{...$getDimensions(item)}
 			{...getProps?.({ value: _y(item), item, index })}
 			{...$$restProps}
 		/>
---
+++ src/lib/utils/rect.ts
@@ -0,0 +1,133 @@
+import { derived } from 'svelte/store';
+import { max, min } from 'd3-array';
+
+import { groupScaleBand, isScaleBand } from './scales';
+
+type DimensionGetterOptions = {
+	/** Override `x` accessor from context */
+	x?: (item: any) => any;
+	/** Override `y` accessor from context */
+	y?: (item: any) => any;
+	groupBy?: string;
+	padding?: number;
+	groupPadding?: { inner?: number; outer?: number };
+};
+
+// TOOD: Pass in overrides for `x` and `y` accessors
+export function createDimensionGetter(context, options?: DimensionGetterOptions) {
+	const {
+		flatData,
+		xGet,
+		yGet,
+		xRange,
+		yRange,
+		xScale,
+		yScale,
+		x: xAccessor,
+		y: yAccessor
+	} = context;
+
+	const groupBy = options?.groupBy;
+	const padding = options?.padding ?? 0;
+
+	return derived(
+		[flatData, xGet, yGet, xRange, yRange, xScale, yScale, xAccessor, yAccessor],
+		([$flatData, $xGet, $yGet, $xRange, $yRange, $xScale, $yScale, $xAccessor, $yAccessor]) => {
+			return function getter(item) {
+				if (isScaleBand($xScale)) {
+					// Vertical
+					const x1Scale = groupBy
+						? groupScaleBand($xScale, $flatData, groupBy, options?.groupPadding)
+						: null;
+
+					const x = $xGet(item) + (x1Scale ? x1Scale(item[groupBy]) : 0) + padding / 2;
+					const width = Math.max(
+						0,
+						(x1Scale ? x1Scale.bandwidth() : $xScale.bandwidth()) - padding
+					);
+
+					const _y = options?.y
+						? typeof options.y === 'string'
+							? (d) => d[options.y]
+							: options?.y
+						: $yAccessor;
+					const yValue = _y(item);
+
+					let top = 0;
+					let bottom = 0;
+					if (Array.isArray(yValue)) {
+						// Array contains both top and bottom values (stack, etc);
+						top = max(yValue);
+						bottom = min(yValue);
+					} else if (yValue == null) {
+						// null/undefined value
+						top = 0;
+						bottom = 0;
+					} else if (yValue > 0) {
+						// Positive value
+						top = yValue;
+						bottom = min($yRange); // or `0`?
+					} else {
+						// Negative value
+						top = min($yRange); // or `0`?
+						bottom = yValue;
+					}
+
+					return {
+						x,
+						y: $yScale(top),
+						width,
+						height: $yScale(bottom) - $yScale(top)
+					};
+				} else if (isScaleBand($yScale)) {
+					// Horizontal
+					const y1Scale = groupBy
+						? groupScaleBand($yScale, $flatData, groupBy, options?.groupPadding)
+						: null;
+					const y = $yGet(item) + (y1Scale ? y1Scale(item[groupBy]) : 0) + padding / 2;
+					const height = Math.max(
+						0,
+						(y1Scale ? y1Scale.bandwidth() : $yScale.bandwidth()) - padding
+					);
+
+					const _x = options?.x
+						? typeof options.x === 'string'
+							? (d) => d[options.x]
+							: options?.x
+						: $xAccessor;
+					const xValue = _x(item);
+
+					let left = 0;
+					let right = 0;
+					if (Array.isArray(xValue)) {
+						// Array contains both top and bottom values (stack, etc);
+						left = min(xValue);
+						right = max(xValue);
+					} else if (xValue == null) {
+						// null/undefined value
+						left = 0;
+						right = 0;
+					} else if (xValue > 0) {
+						// Positive value
+						left = min($xRange); // or `0`?
+						right = xValue;
+					} else {
+						// Negative value
+						left = xValue;
+						right = min($xRange); // or `0`?
+					}
+
+					return {
+						x: $xScale(left),
+						y,
+						width: $xScale(right) - $xScale(left),
+						height
+					};
+				} else {
+					// TODO: Handle both $xScale.bandwidth and $yScale.bandwidth?
+					// TODO: Handle neither $xScale.bandwidth or $yScale.bandwidth? `Math.max(0, $xGet(d)[1] - $xGet(d)[0])` instead of `$xScale.bandwidth()`
+				}
+			};
+		}
+	);
+}
