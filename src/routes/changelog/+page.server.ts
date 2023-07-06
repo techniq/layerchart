e62455fdcf088c3ca1caebb682c82f25a9e2699e
Sean Lynch <techniq35@gmail.com>
2023-07-05 22:17:22 -0400

    Prerender /changelog route

--- src/routes/changelog/+page.server.ts
+++ src/routes/changelog/+page.server.ts
@@ -1,6 +1,8 @@
 import * as fs from 'node:fs/promises';
 
-export async function load({ platform, url }) {
+export let prerender = true;
+
+export async function load() {
   const changelog = await fs.readFile('./CHANGELOG.md', 'utf-8');
 
   return { changelog };
