93ac3db05f26cd0064c9f7ce4fa1c6abfe9dcfeb
Sean Lynch <techniq35@gmail.com>
2023-07-09 00:30:46 -0400

    [RangeField] Move prev/next buttons to prepend/append Field slots

--- src/lib/docs/RangeField.svelte
+++ src/lib/docs/RangeField.svelte
@@ -10,22 +10,28 @@
 </script>
 
 <Field let:id classes={{ input: 'mt-[6px] mb-1' }} {...$$restProps}>
-  <Button
-    icon={mdiChevronLeft}
-    on:click={() => (value -= value > min ? step : 0)}
-    class="mr-2"
-    size="sm"
-  />
+  <span slot="prepend">
+    <Button
+      icon={mdiChevronLeft}
+      on:click={() => (value -= value > min ? step : 0)}
+      class="mr-2"
+      size="sm"
+    />
+  </span>
+
   <input type="range" bind:value {min} {max} {step} {id} class="h-6 w-full" />
   <span class="ml-2 text-sm text-black/50 tabular-nums text-right inline-grid">
     <span class="col-span-full row-span-full invisible">{min}</span>
     <span class="col-span-full row-span-full">{value}</span>
     <span class="col-span-full row-span-full invisible">{max}</span>
   </span>
-  <Button
-    icon={mdiChevronRight}
-    on:click={() => (value += value < max ? step : 0)}
-    class="ml-2"
-    size="sm"
-  />
+
+  <span slot="append">
+    <Button
+      icon={mdiChevronRight}
+      on:click={() => (value += value < max ? step : 0)}
+      class="ml-2"
+      size="sm"
+    />
+  </span>
 </Field>
