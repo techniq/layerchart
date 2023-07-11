405ab98f402845959513c645c3f5e5d32565a356
Sean Lynch <techniq35@gmail.com>
2023-07-11 09:55:28 -0400

    [Blockquote] Small refinements

--- src/lib/docs/Blockquote.svelte
+++ src/lib/docs/Blockquote.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-  class="bg-accent-500/10 border-l-4 border-accent-500 px-4 py-2 border my-4 rounded flex items-center gap-2 text-accent-900"
+  class="bg-accent-500/10 border-l-[6px] border-l-accent-500 border-accent-500/30 text-accent-900 px-4 py-2 border my-4 rounded flex items-center gap-2 text-sm"
 >
   <Icon data={mdiInformation} class="text-accent-500" />
   <slot />
