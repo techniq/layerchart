6b39b7c5c38520f437319e5b5e1f5801429c2194
Sean Lynch <techniq35@gmail.com>
2023-07-11 01:47:03 -0400

    Open AnimatedGlobe example inline first.  Use accent text color for Blockquote

--- src/lib/docs/Blockquote.svelte
+++ src/lib/docs/Blockquote.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-  class="bg-accent-500/10 border-l-4 border-accent-500 px-4 py-2 border my-4 rounded flex items-center gap-2"
+  class="bg-accent-500/10 border-l-4 border-accent-500 px-4 py-2 border my-4 rounded flex items-center gap-2 text-accent-900"
 >
   <Icon data={mdiInformation} class="text-accent-500" />
   <slot />
--- src/routes/docs/examples/AnimatedGlobe/+page.ts
+++ src/routes/docs/examples/AnimatedGlobe/+page.ts
@@ -1,7 +1,12 @@
+import pageSource from './+page.svelte?raw';
+
 export async function load({ fetch }) {
   // TODO: Support different US (https://github.com/topojson/us-atlas) and World (https://github.com/topojson/world-atlas) files
   // TODO: Cache: https://github.com/sveltejs/kit/issues/3642
   return {
-    geojson: (await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')).json()
+    geojson: (await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')).json(),
+    meta: {
+      pageSource
+    }
   };
 }
