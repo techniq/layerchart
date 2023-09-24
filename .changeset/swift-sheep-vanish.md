b9e4722ac544b26ae546d9cccbe91ea10bd8e911
Sean Lynch <techniq35@gmail.com>
2023-09-23 22:46:48 -0400

    Add highlight on hover examples for multi-series Area and Line

---
+++ .changeset/swift-sheep-vanish.md
@@ -0,0 +1,5 @@
+---
+'layerchart': patch
+---
+
+Add highlight on hover examples for multi-series Area and Line
--- src/routes/docs/examples/Area/+page.svelte
+++ src/routes/docs/examples/Area/+page.svelte
@@ -187,6 +187,61 @@
   </div>
 </Preview>
 
+<h2>Multiple series (highlight on hover)</h2>
+
+<Preview>
+  <div class="h-[300px] p-4 border rounded">
+    <Chart
+      data={multiSeriesFlatData}
+      x="date"
+      xScale={scaleTime()}
+      y="value"
+      yDomain={[0, null]}
+      yNice
+      r="fruit"
+      rScale={scaleOrdinal()}
+      rDomain={Object.keys(fruitColors)}
+      rRange={Object.values(fruitColors)}
+      padding={{ left: 16, bottom: 24, right: 48 }}
+      tooltip={{ mode: 'voronoi' }}
+      let:tooltip
+      let:rScale
+    >
+      <Svg>
+        <Axis placement="left" grid rule />
+        <Axis placement="bottom" format={(d) => formatDate(d, PeriodType.Day, 'short')} rule />
+        {#each dataByFruit as [fruit, data]}
+          {@const color =
+            tooltip.data == null || tooltip.data.fruit === fruit ? rScale(fruit) : '#ddd'}
+          <Area
+            {data}
+            fill={color}
+            fill-opacity={0.3}
+            line={{ class: 'stroke-2', stroke: color }}
+          />
+          <Point d={data[data.length - 1]} let:x let:y>
+            <circle cx={x} cy={y} r={4} fill={color} />
+            <Text
+              {x}
+              {y}
+              value={fruit}
+              verticalAnchor="middle"
+              dx={6}
+              dy={-2}
+              class="text-xs"
+              fill={color}
+            />
+          </Point>
+        {/each}
+        <Highlight points lines />
+      </Svg>
+      <Tooltip header={(data) => format(data.date, 'eee, MMMM do')} let:data>
+        <TooltipItem label={data.fruit} value={data.value} />
+      </Tooltip>
+    </Chart>
+  </div>
+</Preview>
+
 <h2>Multiple series with labels</h2>
 
 <Preview>
