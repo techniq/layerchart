4df888fc2093f4229fec104a4f99a86dcf4ad3d8
Matt Langeman <matt@langeman.net>
2023-10-31 10:39:53 -0400

    add getting started details to the home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { marked } from 'marked';
+  import { TableOfContents } from 'svelte-ux';
+
+  import markdownString from './getting-started.md?raw';
+
+  function sanitize(str: string) {
+    return str.replace(/</g, '\\<').replace(/>/g, '\\>');
+  }
+</script>
+
+<div class="grid grid-cols-[1fr,auto] gap-6 pt-2 pb-4">
+  <div class="prose max-w-none px-4 bg-white p-2 m-2 rounded shadow-lg border overflow-auto">
+    {@html marked.parse(sanitize(markdownString))}
+  </div>
+
+  <div class="hidden lg:block w-[224px]">
+    <div class="sticky top-0 pr-2 max-h-[calc(100vh-64px)] overflow-auto">
+      <div class="text-xs uppercase leading-8 tracking-widest text-black/50">On this page</div>
+      <TableOfContents maxDepth={2} />
+    </div>
+  </div>
+</div>
