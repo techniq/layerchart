a08913e14273b1b9b99c5d18e8a0931ec32264c4
Sean Lynch <techniq35@gmail.com>
2023-11-07 20:42:15 -0500

    Cleanup

--- src/routes/docs/utils/pivot/+page.svelte
+++ src/routes/docs/utils/pivot/+page.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
   import Json from '$lib/docs/Json.svelte';
-  import Preview from '$lib/docs/Preview.svelte';
   import { pivotLonger, pivotWider } from '$lib/utils/pivot';
   import { wideData, longData } from '$lib/utils/genData';
   import Code from '$lib/docs/Code.svelte';
-
-  const wideDataDisplay = JSON.stringify(wideData, null, 2);
-  const longDataDisplay = JSON.stringify(longData, null, 2);
-
-  const pivotLongerResult = pivotLonger(
-    wideData,
-    ['apples', 'bananas', 'cherries', 'dates'],
-    'fruit',
-    'value'
-  );
-  const pivotLongerDisplay = JSON.stringify(pivotLongerResult, null, 2);
-
-  const pivotWiderResult = pivotWider(longData, 'year', 'fruit', 'value');
-  const pivotWiderDisplay = JSON.stringify(pivotWiderResult, null, 2);
 </script>
 
 <h1>pivotLonger</h1>
