760fd66eb061a770ed58c33579df6e4b715fe6dd
Sean Lynch <techniq35@gmail.com>
2023-11-07 22:45:15 -0500

    Yakko's song WIP

--- src/routes/docs/examples/AnimatedGlobe/+page.svelte
+++ src/routes/docs/examples/AnimatedGlobe/+page.svelte
@@ -2,16 +2,32 @@
   import { spring } from 'svelte/motion';
   import { geoOrthographic, geoCentroid } from 'd3-geo';
   import { feature } from 'topojson-client';
+  import { index } from 'd3-array';
 
-  import { Button, scrollIntoView, cls, sortFunc } from 'svelte-ux';
+  import { mdiPlay, mdiStop } from '@mdi/js';
+
+  import {
+    Button,
+    scrollIntoView,
+    cls,
+    sortFunc,
+    ButtonGroup,
+    timerStore,
+    Duration,
+  } from 'svelte-ux';
 
   import Preview from '$lib/docs/Preview.svelte';
   import Chart, { Canvas, Svg } from '$lib/components/Chart.svelte';
   import GeoPath from '$lib/components/GeoPath.svelte';
   import Graticule from '$lib/components/Graticule.svelte';
   import Tooltip from '$lib/components/Tooltip.svelte';
+  import { timings } from './song.js';
 
   export let data;
+
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   const countries = feature(data.geojson, data.geojson.objects.countries);
 
@@ -31,6 +47,40 @@
   // https://animaniacs.fandom.com/wiki/Yakko%27s_World_(song)#New_Updated_Verse
   // https://www.youtube.com/watch?v=BoaLSUKeGWw
   // https://www.youtube.com/watch?v=5pOFKmk7ytU
+
+  const audioFile = new Audio('/audio/yakko_world.mp3');
+  const audioCurrentTime = timerStore({ initial: 0, onTick: () => audioFile.currentTime });
+
+  const countryFeaturesByName = index(countries.features, (f) => f.properties.name);
+
+  const skipIntro = true;
+  let isPlaying = false;
+
+  async function play() {
+    isPlaying = true;
+    audioFile.currentTime = skipIntro ? 10 : 0;
+    audioFile.play();
+
+    for (const timing of timings) {
+      if (skipIntro) {
+        delay(10000);
+      }
+
+      await delay(timing.delay);
+
+      if (!isPlaying) {
+        return;
+      }
+      selectedFeature = countryFeaturesByName.get(timing.name);
+    }
+  }
+
+  function stop() {
+    isPlaying = false;
+    audioFile.pause();
+    audioFile.currentTime = 0;
+    selectedFeature = null;
+  }
 </script>
 
 <h1>Examples</h1>
@@ -38,7 +88,19 @@
 <h2>SVG</h2>
 
 <Preview data={countries}>
-  <div class="h-[600px] grid grid-cols-[224px,1fr]">
+  <div class="h-[600px] grid grid-cols-[224px,1fr] relative">
+    <div class="absolute top-0 left-0 bg-gray-100 w-full text-lg font-semibold">
+      {selectedFeature?.properties.name ?? ''}
+    </div>
+
+    <div class="absolute top-0 right-0 z-10 flex items-center gap-3">
+      <!-- <Duration duration={{ seconds: $audioCurrentTime }} class="text-sm text-black/50" /> -->
+      <ButtonGroup variant="fill-light" color="blue" size="sm">
+        <Button icon={mdiPlay} on:click={play} />
+        <Button icon={mdiStop} on:click={stop} />
+      </ButtonGroup>
+    </div>
+
     <div class="overflow-auto scrollbar-none">
       {#each countries.features.sort(sortFunc('properties.name')) as country}
         {@const isSelected = selectedFeature === country}
@@ -93,8 +155,20 @@
 <h2>Canvas</h2>
 
 <Preview data={countries}>
-  <div class="h-[600px] grid grid-cols-[224px,1fr]">
-    <div class="overflow-auto">
+  <div class="h-[600px] grid grid-cols-[224px,1fr] relative">
+    <div class="absolute top-0 left-0 bg-gray-100 w-full text-lg font-semibold">
+      {selectedFeature?.properties.name ?? ''}
+    </div>
+
+    <div class="absolute top-0 right-0 z-10 flex items-center gap-3">
+      <!-- <Duration duration={{ seconds: $audioCurrentTime }} class="text-sm text-black/50" /> -->
+      <ButtonGroup variant="fill-light" color="blue" size="sm">
+        <Button icon={mdiPlay} on:click={play} />
+        <Button icon={mdiStop} on:click={stop} />
+      </ButtonGroup>
+    </div>
+
+    <div class="overflow-auto scrollbar-none">
       {#each countries.features.sort(sortFunc('properties.name')) as country}
         {@const isSelected = selectedFeature === country}
         <div use:scrollIntoView={{ condition: isSelected }}>
