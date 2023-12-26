bb113fba4c57738311529334c3a5ceacdacdd6f9
Sean Lynch <techniq35@gmail.com>
2023-12-26 10:58:10 -0500

    Update Svelte UX and use tailwind config themes instead of purely CSS

--- packages/layerchart/tailwind.config.cjs
+++ packages/layerchart/tailwind.config.cjs
@@ -1,8 +1,24 @@
 const colors = require('tailwindcss/colors');
 const plugin = require('tailwindcss/plugin');
 
+const { themes } = require('svelte-ux/styles/daisy');
+
 module.exports = {
   content: ['./src/**/*.{html,svelte,md}', './node_modules/svelte-ux/**/*.{svelte,js,md}'],
+  ux: {
+    themes,
+    // themes: {
+    //   light: {
+    //     primary: colors['blue']['500'],
+    //     'primary-content': 'white',
+    //     secondary: colors['cyan']['300'],
+    //     'surface-100': 'white',
+    //     'surface-200': colors['gray']['100'],
+    //     'surface-300': colors['gray']['300'],
+    //     'surface-content': colors['gray']['900'],
+    //   },
+    // },
+  },
   theme: {
     extend: {},
   },
