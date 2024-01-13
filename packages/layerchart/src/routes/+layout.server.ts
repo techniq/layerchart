a17e69adcb5adf33bd6eeb6fdb0d80cbdf7ba8a9
Sean Lynch <techniq35@gmail.com>
2024-01-12 21:18:23 -0500

    Comment out `$env/dynamic/private` usage unti SvelteKit is upgraded to `2.3.2+` to fix Safari issue

--- packages/layerchart/src/routes/+layout.server.ts
+++ packages/layerchart/src/routes/+layout.server.ts
@@ -1,4 +1,4 @@
-import { env } from '$env/dynamic/private';
+// import { env } from '$env/dynamic/private';
 
 import themes from '../../themes.json';
 import { getThemeNames } from 'svelte-ux/styles/theme';
@@ -6,6 +6,6 @@ import { getThemeNames } from 'svelte-ux/styles/theme';
 export async function load() {
   return {
     themes: getThemeNames(themes),
-    pr_id: env.VERCEL_GIT_PULL_REQUEST_ID,
+    // pr_id: env.VERCEL_GIT_PULL_REQUEST_ID, // TODO: Re-add once SvelteKit updated to `2.3.2+` - https://github.com/sveltejs/kit/releases/tag/%40sveltejs%2Fkit%402.3.2
   };
 }
