e6c20a940a12102b4042f3bf076f2430deba0a32
Sean Lynch <techniq35@gmail.com>
2024-02-19 00:03:27 -0500

    Add polarToCartesian() / cartesianToPolar() coordinate system utils

--- packages/layerchart/src/lib/utils/math.ts
+++ packages/layerchart/src/lib/utils/math.ts
@@ -11,3 +11,26 @@ export function degreesToRadians(degrees: number) {
 export function radiansToDegrees(radians: number) {
   return radians * (180 / Math.PI);
 }
+
+/**
+ * Convert polar to cartesian coordinate system.
+ * see also: https://d3js.org/d3-shape/symbol#pointRadial
+ * @param angle - Angle in radians
+ * @param radius - Radius
+ */
+export function polarToCartesian(angle: number, radius: number) {
+  return {
+    x: radius * Math.cos(angle),
+    y: radius * Math.sin(angle),
+  };
+}
+
+/**
+ * Convert cartesian to polar coordinate system.  Angle in radians
+ */
+export function cartesianToPolar(x: number, y: number) {
+  return {
+    radius: Math.sqrt(x ** 2 + y ** 2),
+    angle: Math.atan(y / x),
+  };
+}
