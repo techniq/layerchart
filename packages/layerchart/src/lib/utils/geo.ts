e8c97a125a627b46b9d7cea0c1f25a3e5df26b9e
Sean Lynch <techniq35@gmail.com>
2024-04-04 23:36:14 -0400

    [GeoPath] Support `curve`

--- packages/layerchart/src/lib/utils/geo.ts
+++ packages/layerchart/src/lib/utils/geo.ts
@@ -1,3 +1,61 @@
+import {
+  geoPath as d3geoPath,
+  type GeoContext,
+  type GeoPermissibleObjects,
+  type GeoProjection,
+  type GeoStreamWrapper,
+} from 'd3-geo';
+import { path, type Path } from 'd3-path';
+
+import { type CurveFactory, type CurveFactoryLineOnly } from 'd3-shape';
+
+/**
+ * Render a geoPath() using curve factory
+ * @see {@link https://observablehq.com/@d3/context-to-curve}
+ */
+export function geoCurvePath(
+  projection: GeoProjection | GeoStreamWrapper | null,
+  curve: CurveFactory | CurveFactoryLineOnly,
+  context?: CanvasRenderingContext2D | Path
+) {
+  const pathContext = context === undefined ? path() : context;
+  const geoPath = d3geoPath(projection, curveContext(curve(pathContext)));
+
+  const fn = (object: GeoPermissibleObjects) => {
+    geoPath(object);
+    return context === undefined ? pathContext + '' : undefined;
+  };
+
+  // Expose geoPath properties such as `.centroid()`
+  Object.setPrototypeOf(fn, geoPath);
+
+  return fn;
+}
+
+/**
+ * Translate Curve to GeoContext interface
+ */
+function curveContext(curve: ReturnType<CurveFactory | CurveFactoryLineOnly>): GeoContext {
+  return {
+    beginPath() {
+      // nothing?
+    },
+    moveTo(x, y) {
+      curve.lineStart();
+      curve.point(x, y);
+    },
+    arc(x, y, radius, startAngle, endAngle, anticlockwise) {
+      // nothing?
+    },
+    lineTo(x, y) {
+      curve.point(x, y);
+    },
+    closePath() {
+      curve.lineEnd();
+    },
+  };
+}
+
 /**
  * Return the point on Earth's surface that is diametrically opposite to another point
  * @see: https://en.wikipedia.org/wiki/Antipodes
