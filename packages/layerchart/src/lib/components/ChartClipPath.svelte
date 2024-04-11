eca9d4a8fc3628e626f7ecab21c4ea8753bcce05
Sean Lynch <techniq35@gmail.com>
2024-04-11 11:12:47 -0400

    [ChartClipPath] Remove padding by default (opt-in with `includePadding`)

--- packages/layerchart/src/lib/components/ChartClipPath.svelte
+++ packages/layerchart/src/lib/components/ChartClipPath.svelte
@@ -4,13 +4,16 @@
   import RectClipPath from './RectClipPath.svelte';
 
   const { width, height, padding } = getContext('LayerCake');
+
+  /** Whether clipping should include chart padding (ex. axis) */
+  export let includePadding = false;
 </script>
 
 <RectClipPath
-  x={-$padding.left}
-  y={-$padding.top}
-  width={$width + $padding.left + $padding.right}
-  height={$height + $padding.top + $padding.bottom}
+  x={includePadding ? -$padding.left : 0}
+  y={includePadding ? -$padding.top : 0}
+  width={$width + (includePadding ? $padding.left + $padding.right : 0)}
+  height={$height + (includePadding ? $padding.top + $padding.bottom : 0)}
   on:click
   {...$$restProps}
 >
