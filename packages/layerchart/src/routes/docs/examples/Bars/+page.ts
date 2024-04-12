bb754bc4dd7fa4aafda602a4b33435c78ce18537
Sean Lynch <techniq35@gmail.com>
2024-04-11 21:57:46 -0400

    Update vertical/horizontal bar chart titles

--- packages/layerchart/src/routes/docs/examples/Bars/+page.ts
+++ packages/layerchart/src/routes/docs/examples/Bars/+page.ts
@@ -3,6 +3,7 @@ import pageSource from './+page.svelte?raw';
 export async function load() {
   return {
     meta: {
+      title: 'Bar Chart (Horizontal)',
       pageSource,
       related: ['components/Bars', 'examples/Columns', 'examples/Histogram'],
     },
--- packages/layerchart/src/routes/docs/examples/Columns/+page.ts
+++ packages/layerchart/src/routes/docs/examples/Columns/+page.ts
@@ -3,6 +3,7 @@ import pageSource from './+page.svelte?raw';
 export async function load() {
   return {
     meta: {
+      title: 'Bar Chart (Vertical)',
       pageSource,
       related: ['components/Bars', 'examples/Bars', 'examples/Histogram', 'examples/Sparkbar'],
     },
