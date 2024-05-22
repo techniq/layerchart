05805a86870c1bc8893566898f5d1fc286b93d91
Sean Lynch <techniq35@gmail.com>
2024-05-22 08:05:57 -0400

    Add examples with debug view to HitCanvas docs page.  Link additional related

--- packages/layerchart/src/routes/docs/components/HitCanvas/+page.svelte
+++ packages/layerchart/src/routes/docs/components/HitCanvas/+page.svelte
@@ -1,6 +1,79 @@
 <script lang="ts">
-  import Chart, { Svg } from '$lib/components/Chart.svelte';
+  import { geoIdentity } from 'd3-geo';
+  import { feature } from 'topojson-client';
 
   import Preview from '$lib/docs/Preview.svelte';
-  import { getSpiral } from '$lib/utils/genData.js';
+  import Chart, { Canvas } from '$lib/components/Chart.svelte';
+  import GeoPath from '$lib/components/GeoPath.svelte';
+  import HitCanvas from '$lib/components/HitCanvas.svelte';
+  import Tooltip from '$lib/components/Tooltip.svelte';
+  import { Field, Switch } from 'svelte-ux';
+
+  export let data;
+
+  const states = feature(data.geojson, data.geojson.objects.states);
+  const counties = feature(data.geojson, data.geojson.objects.counties);
+
+  let debug = false;
 </script>
+
+<div class="grid grid-cols-[1fr,auto] gap-2 items-end">
+  <h2>Canvas</h2>
+
+  <div class="mb-2 flex gap-6">
+    <Field label="Debug:" dense labelPlacement="left" let:id>
+      <Switch {id} bind:checked={debug} />
+    </Field>
+  </div>
+</div>
+
+<Preview data={states}>
+  <div class="h-[600px] mt-10">
+    <Chart
+      geo={{
+        projection: geoIdentity,
+        fitGeojson: states,
+      }}
+      tooltip={{ mode: 'manual' }}
+      let:tooltip
+    >
+      <Canvas>
+        <GeoPath geojson={states} class="stroke-surface-content" />
+      </Canvas>
+      <Canvas>
+        <GeoPath geojson={counties} class="stroke-surface-content/20" />
+      </Canvas>
+
+      {#if tooltip.data}
+        <Canvas>
+          <GeoPath geojson={tooltip.data} class="stroke-surface-content fill-surface-content/20" />
+        </Canvas>
+      {/if}
+
+      <HitCanvas
+        let:nextColor
+        let:setColorData
+        on:mousemove={(e) => tooltip.show(e.detail.event, e.detail.data)}
+        on:mouseleave={tooltip.hide}
+        {debug}
+      >
+        <GeoPath
+          render={(ctx, { geoPath }) => {
+            for (var feature of counties.features) {
+              const color = nextColor();
+
+              ctx.beginPath();
+              geoPath(feature);
+              ctx.fillStyle = color;
+              ctx.fill();
+
+              setColorData(color, feature);
+            }
+          }}
+        />
+      </HitCanvas>
+
+      <Tooltip header={(data) => data.properties.name} />
+    </Chart>
+  </div>
+</Preview>
--- packages/layerchart/src/routes/docs/components/HitCanvas/+page.ts
+++ packages/layerchart/src/routes/docs/components/HitCanvas/+page.ts
@@ -4,12 +4,21 @@ import pageSource from './+page.svelte?raw';
 
 export async function load() {
   return {
+    geojson: await fetch('https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json').then(
+      (r) => r.json()
+    ),
     meta: {
       api,
       source,
       pageSource,
-      description: 'Hidden color-coded canvas lookup to enable shape-based tooltips (ex. GeoPath)',
-      related: ['examples/GeoPath'],
+      description:
+        'Hidden color-coded lookup to enable shape-based tooltips when using Canvas (ex. GeoPath)',
+      related: [
+        'examples/GeoPath',
+        'examples/Choropleth',
+        'examples/BubbleMap',
+        'examples/SpikeMap',
+      ],
     },
   };
 }
