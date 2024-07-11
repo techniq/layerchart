6d7ad7c81d64991574cd9e9577e2f4dd52e63ec4
Sean Lynch <techniq35@gmail.com>
2024-07-10 23:49:29 -0400

    Add another DualAxis example using multiple, stacked charts (simplier without a remapping scale, but a little more verbose and difficult to show highlight points)

--- packages/layerchart/src/routes/docs/examples/DualAxis/+page.svelte
+++ packages/layerchart/src/routes/docs/examples/DualAxis/+page.svelte
@@ -19,46 +19,7 @@
 
 <h1>Examples</h1>
 
-<h2>Basic</h2>
-
-<Preview data={chartData}>
-  <div class="h-[300px] p-4 border rounded">
-    <Chart
-      data={chartData}
-      x="year"
-      y="sales"
-      yDomain={[0, null]}
-      yNice
-      padding={{ top: 24, bottom: 24, left: 24, right: 24 }}
-      let:height
-    >
-      <Svg>
-        <Axis
-          placement="left"
-          rule
-          format="metric"
-          label="↑ sales (M)"
-          labelPlacement="start"
-          labelProps={{ class: 'fill-primary' }}
-        />
-        <Axis
-          placement="right"
-          rule
-          label="efficiency (mpg) ↑"
-          labelPlacement="start"
-          scale={scaleLinear(efficiencyScale.domain(), [height, 0])}
-          ticks={efficiencyScale.ticks()}
-          labelProps={{ class: 'fill-secondary' }}
-        />
-        <Axis placement="bottom" format="none" rule />
-        <Spline class="stroke-2 stroke-primary" />
-        <Spline y={(d) => efficiencyScale(d.efficiency)} class="stroke-2 stroke-secondary" />
-      </Svg>
-    </Chart>
-  </div>
-</Preview>
-
-<h2>With Tooltip and Highlight</h2>
+<h2>Single chart with remapping scale</h2>
 
 <Preview data={chartData}>
   <div class="h-[300px] p-4 border rounded">
@@ -95,6 +56,63 @@
         <Spline y={(d) => efficiencyScale(d.efficiency)} class="stroke-2 stroke-secondary" />
         <Highlight lines points />
         <Highlight points={{ class: 'fill-secondary' }} y={(d) => efficiencyScale(d.efficiency)} />
+      </Svg>
+
+      <Tooltip header={(data) => data.year} let:data>
+        <TooltipItem label="sales" value={data.sales} format="currencyRound" />
+        <TooltipItem label="efficiency" value={data.efficiency} />
+      </Tooltip>
+    </Chart>
+  </div>
+</Preview>
+
+<h2>Stacked Charts</h2>
+
+<Preview data={chartData}>
+  <div class="h-[300px] grid grid-stack p-4 border rounded">
+    <!-- Sales chart-->
+    <Chart
+      data={chartData}
+      x="year"
+      y="sales"
+      yDomain={[0, null]}
+      yNice
+      padding={{ top: 24, bottom: 24, left: 24, right: 24 }}
+    >
+      <Svg>
+        <Axis
+          placement="left"
+          rule
+          format="metric"
+          label="↑ sales (M)"
+          labelPlacement="start"
+          labelProps={{ class: 'fill-primary' }}
+        />
+        <Axis placement="bottom" format="none" rule />
+        <Spline class="stroke-2 stroke-primary" />
+        <Highlight lines points />
+      </Svg>
+    </Chart>
+
+    <!-- Efficiency chart, provides tooltip for both values  -->
+    <Chart
+      data={chartData}
+      x="year"
+      y="efficiency"
+      padding={{ top: 24, bottom: 24, left: 24, right: 24 }}
+      tooltip={{ mode: 'bisect-x' }}
+    >
+      <Svg>
+        <Axis
+          placement="right"
+          rule
+          label="efficiency (mpg) ↑"
+          labelPlacement="start"
+          labelProps={{ class: 'fill-secondary' }}
+        />
+        <Spline class="stroke-2 stroke-secondary" />
+        <!-- Difficult to add points for both charts without using a remaped scale for one value -->
+        <Highlight lines />
       </Svg>
 
       <Tooltip header={(data) => data.year} let:data>
