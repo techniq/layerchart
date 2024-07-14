c15cd879ec8bdbb541faf68083a1c287c0a9540e
Sean Lynch <techniq35@gmail.com>
2024-07-14 01:32:19 -0400

    [TooltipItem] Support `color` prop to add swatch, support theme colors and any color via `color="variable"`

--- packages/layerchart/src/lib/components/TooltipItem.svelte
+++ packages/layerchart/src/lib/components/TooltipItem.svelte
@@ -1,32 +1,57 @@
 <script lang="ts">
-  import { cls, format as formatUtil, type FormatType } from 'svelte-ux';
+  import { cls, format as formatUtil, type FormatType, type ThemeColors } from 'svelte-ux';
 
   export let label: any;
   /** Value to be formatted and displayed.  Can also be passed as default slot */
   export let value: any = undefined;
   export let format: FormatType | undefined = undefined;
   export let valueAlign: 'left' | 'right' | 'center' = 'left';
+  export let color: ThemeColors | 'variable' | undefined = undefined;
 
   export let classes: {
+    root?: string;
     label?: string;
     value?: string;
+    color?: string;
   } = {};
 </script>
 
-<div class={cls('label', classes.label)}>
-  <slot name="label">{label}:</slot>
-</div>
+<div class={cls('contents', classes.root, $$props.class)} {...$$restProps}>
+  <div class={cls('label', 'flex items-center gap-2', classes.label)}>
+    {#if color}
+      <div
+        class={cls(
+          'color',
+          'inline-block size-2 rounded-full',
+          {
+            primary: 'bg-primary',
+            secondary: 'bg-secondary',
+            accent: 'bg-accent',
+            neutral: 'bg-neutral',
+            info: 'bg-info',
+            success: 'bg-success',
+            warning: 'bg-warning',
+            danger: 'bg-danger',
+            variable: 'bg-[--color]',
+          }[color.toString()],
+          classes.color
+        )}
+      ></div>
+    {/if}
+    <slot name="label">{label}</slot>
+  </div>
 
-<div
-  class={cls(
-    'value',
-    {
-      'text-right': valueAlign === 'right',
-      'text-center': valueAlign === 'center',
-    },
-    classes.value,
-    $$props.class
-  )}
->
-  <slot>{format ? formatUtil(value, format) : value}</slot>
+  <div
+    class={cls(
+      'value',
+      {
+        'text-right': valueAlign === 'right',
+        'text-center': valueAlign === 'center',
+      },
+      classes.value,
+      $$props.class
+    )}
+  >
+    <slot>{format ? formatUtil(value, format) : value}</slot>
+  </div>
 </div>
