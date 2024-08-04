7438c638bde2498260191e7810b4684ca5a5ddc3
Sean Lynch <techniq35@gmail.com>
2024-08-04 16:08:46 -0400

    [TooltipSeparator] Use theme color

--- packages/layerchart/src/lib/components/tooltip/TooltipSeparator.svelte
+++ packages/layerchart/src/lib/components/tooltip/TooltipSeparator.svelte
@@ -2,4 +2,4 @@
   import { cls } from 'svelte-ux';
 </script>
 
-<div class={cls('rounded bg-white/50 my-1 col-span-full h-px', $$props.class)} />
+<div class={cls('rounded bg-surface-content/20 my-1 col-span-full h-px', $$props.class)} />
