d359712606c5374e9a0f9256d2b505616fda1310
Sean Lynch <techniq35@gmail.com>
2024-08-15 13:19:27 -0400

    Remove old graph data file

--- packages/layerchart/src/routes/docs/examples/Sankey/+page.ts
+++ packages/layerchart/src/routes/docs/examples/Sankey/+page.ts
@@ -6,6 +6,7 @@ export async function load() {
     complex: await fetch('/data/examples/graph/complex.json').then((r) => r.json()),
     greenhouse: await fetch('/data/examples/graph/greenhouse.json').then((r) => r.json()),
     flare: await fetch('/data/examples/hierarchy/flare.json').then((r) => r.json()),
+
     meta: {
       pageSource,
       related: ['components/Sankey', 'components/Link'],
