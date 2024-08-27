eeb3032d056c936cfd4e0f284a4113873e83b54c
Sean Lynch <techniq35@gmail.com>
2024-08-27 08:50:35 -0400

    docs(Chart): Improve description

--- packages/layerchart/src/routes/docs/components/Chart/+page.ts
+++ packages/layerchart/src/routes/docs/components/Chart/+page.ts
@@ -9,7 +9,7 @@ export async function load() {
       source,
       pageSource,
       description:
-        'Sets up chart context, including tooltip and geo contexts.  See also simplified charts such as AreaChart and BarChart for streamline configuration',
+        'Sets up chart context, including dimension scales and additional contexts such as `TooltipContext`, `GeoContext`, and `TransformContext`. See also simplified charts such as `AreaChart` and `BarChart` for streamline configuration',
       features: ['Adds support for x and y baselines (always show 0, etc)'],
       related: [
         'components/AreaChart',
