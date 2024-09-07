adea4cfabf4002360ae604553e96054d34dbe090
Sean Lynch <techniq35@gmail.com>
2024-09-07 01:02:44 -0400

    feat(TooltipHeader): Support color swatch

--- packages/layerchart/src/lib/components/tooltip/TooltipHeader.svelte
+++ packages/layerchart/src/lib/components/tooltip/TooltipHeader.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
   import { cls } from '@layerstack/tailwind';
+
+  export let color: string | undefined = undefined;
+
+  export let classes: {
+    root?: string;
+    color?: string;
+  } = {};
 </script>
 
 <div
-  class={cls('TooltipHeader', 'font-semibold whitespace-nowrap border-b mb-1 pb-1', $$props.class)}
+  class={cls(
+    'TooltipHeader',
+    'font-semibold whitespace-nowrap border-b mb-1 pb-1 flex items-center gap-2',
+    classes.root,
+    $$props.class
+  )}
 >
+  {#if color}
+    <div
+      class={cls('color', 'inline-block size-2 rounded-full bg-[--color]', classes.color)}
+      style:--color={color}
+    ></div>
+  {/if}
   <slot />
 </div>
