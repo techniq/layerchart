8ff333bb9de3a4a33d102567069fcdcede97a7fa
Sean Lynch <techniq35@gmail.com>
2024-09-07 01:02:08 -0400

    Add pengiun example dataset

--- packages/layerchart/src/routes/docs/components/ScatterChart/+page.ts
+++ packages/layerchart/src/routes/docs/components/ScatterChart/+page.ts
@@ -1,9 +1,16 @@
+import { autoType, csvParse } from 'd3-dsv';
+
 import api from '$lib/components/charts/ScatterChart.svelte?raw&sveld';
 import source from '$lib/components/charts/ScatterChart.svelte?raw';
 import pageSource from './+page.svelte?raw';
 
+import type { PenguinsData } from '$static/data/examples/penguins.js';
+
 export async function load() {
   return {
+    penguins: (await fetch('/data/examples/penguins.csv').then(async (r) =>
+      csvParse(await r.text(), autoType)
+    )) as PenguinsData,
     meta: {
       api,
       source,
