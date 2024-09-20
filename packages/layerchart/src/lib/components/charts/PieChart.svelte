f57b640eb1244acbbc65742c5cf4ed668bf88ff9
Sean Lynch <techniq35@gmail.com>
2024-09-20 01:08:53 -0400

    feat(PieChart): Add `series` support (and remove `separateTracks`)

--- packages/layerchart/src/lib/components/charts/PieChart.svelte
+++ packages/layerchart/src/lib/components/charts/PieChart.svelte
@@ -1,6 +1,5 @@
 <script lang="ts" generics="TData">
   import { type ComponentProps } from 'svelte';
-  import { scaleOrdinal } from 'd3-scale';
   import { sum } from 'd3-array';
   import { format } from '@layerstack/utils';
 
@@ -17,7 +16,7 @@
     TODO:
     - [ ] Centroid labels
     - [ ] Outside/offset labels
-    - [ ] Multiple/concentric charts (data prop, with different domains) - http://localhost:3002/docs/examples/Arc#concentric
+    - [x] Multiple/concentric charts (data prop, with different domains) - http://localhost:3002/docs/examples/Arc#concentric
     - [ ] Segmented arcs - http://localhost:3002/docs/examples/Arc#segmented_arc
   */
 
@@ -30,14 +29,12 @@
     cornerRadius?: typeof cornerRadius;
     padAngle?: typeof padAngle;
     placement?: typeof placement;
-    separateTracks?: typeof separateTracks;
     props?: typeof props;
-    // series?: typeof series;
+    series?: typeof series;
     // labels?: typeof labels;
   }
 
   export let data: $$Props['data'] = [];
-  $: chartData = chartDataArray(data);
 
   /** Label accessor */
   export let label: Accessor<TData> = 'label';
@@ -46,6 +43,18 @@
   /** Value accessor */
   export let value: Accessor<TData> = 'value';
   $: valueAccessor = accessor(value);
+
+  export let series: {
+    key: string | number;
+    label?: string;
+    value?: Accessor<TData>;
+    /** Provider series data, else uses chart data (with value/key accessor) */
+    data?: TData | TData[];
+    /** Maximum possible value, required when `data` is single item */
+    total?: number;
+    color?: string;
+    props?: Partial<ComponentProps<Arc>>;
+  }[] = [{ key: 'default', value: value /*, color: 'hsl(var(--color-primary))'*/ }];
 
   /**
    * Range [min,max] in degrees.  See also startAngle/endAngle
@@ -71,35 +80,42 @@
 
   export let placement: 'left' | 'center' | 'right' | 'none' = 'center';
 
-  /**
-   * Show values as individual, concentric arcs
-   */
-  export let separateTracks = false;
-
-  export let colorScale = scaleOrdinal();
-  export let colorKeys = [...new Set(chartDataArray(data).map((d) => accessor(label)(d)))];
-  export let colors = [
-    'hsl(var(--color-info))',
-    'hsl(var(--color-success))',
-    'hsl(var(--color-warning))',
-    'hsl(var(--color-danger))',
-  ];
-
   export let props: {
     pie?: Partial<ComponentProps<Pie>>;
     group?: Partial<ComponentProps<Group>>;
     arc?: Partial<ComponentProps<Arc>>;
   } = {};
+
+  $: allSeriesData = series
+    .flatMap((s) =>
+      Array.isArray(s.data)
+        ? s.data.map((d) => ({ seriesKey: s.key, ...d }))
+        : s.data
+          ? [s.data]
+          : null
+    )
+    .filter((d) => d) as Array<TData & { stackData?: any }>;
+
+  $: chartData = (allSeriesData.length ? allSeriesData : chartDataArray(data)) as Array<TData>;
+
+  $: seriesColors = series.map((s) => s.color).filter((d) => d != null);
 </script>
 
 <Chart
-  {data}
+  data={chartData}
   x={value}
   y={label}
-  r={label}
-  cScale={colorScale}
-  cDomain={colorKeys}
-  cRange={colors}
+  c={label}
+  cRange={seriesColors.length
+    ? seriesColors
+    : [
+        'hsl(var(--color-primary))',
+        'hsl(var(--color-secondary))',
+        'hsl(var(--color-info))',
+        'hsl(var(--color-success))',
+        'hsl(var(--color-warning))',
+        'hsl(var(--color-danger))',
+      ]}
   {...$$restProps}
   let:x
   let:xScale
@@ -118,52 +134,54 @@
       <slot name="below-marks" {...slotProps} />
 
       <slot name="marks" {...slotProps}>
-        <Pie
-          {range}
-          {innerRadius}
-          {outerRadius}
-          {cornerRadius}
-          {padAngle}
-          {placement}
-          {...props.pie}
-          let:arcs
-        >
-          <Group {...props.group}>
-            {#if separateTracks}
-              {@const sumValue = Number(sum(chartData, valueAccessor))}
-              {#each chartData as d, i}
-                <Arc
-                  value={valueAccessor(d)}
-                  domain={[0, sumValue]}
-                  outerRadius={i * (outerRadius ?? 0)}
-                  {innerRadius}
-                  {cornerRadius}
-                  {padAngle}
-                  fill={cScale(c(d))}
-                  track={{ fill: cScale(c(d)), 'fill-opacity': 0.1 }}
-                  {tooltip}
-                  data={d}
-                  {...props.arc}
-                />
-              {/each}
+        {#each series as s, i}
+          <!-- Redundant logic to make Typescript happy -->
+          <Group center={s.data && !Array.isArray(s.data)} {...props.group}>
+            {#if s.data && !Array.isArray(s.data)}
+              <Arc
+                value={valueAccessor(s.data)}
+                domain={[0, s.total ?? sum(chartData, valueAccessor)]}
+                {range}
+                {innerRadius}
+                outerRadius={i * (outerRadius ?? 0)}
+                {cornerRadius}
+                {padAngle}
+                fill={s.color ?? cScale(c(s.data))}
+                track={{ fill: s.color ?? cScale(c(s.data)), 'fill-opacity': 0.1 }}
+                {tooltip}
+                data={s.data}
+                {...props.arc}
+              />
             {:else}
-              {#each arcs as arc}
-                <Arc
-                  startAngle={arc.startAngle}
-                  endAngle={arc.endAngle}
-                  {outerRadius}
-                  {innerRadius}
-                  {cornerRadius}
-                  {padAngle}
-                  fill={cScale(c(arc.data))}
-                  data={arc.data}
-                  {tooltip}
-                  {...props.arc}
-                />
-              {/each}
+              <Pie
+                data={s.data}
+                {range}
+                {innerRadius}
+                {outerRadius}
+                {cornerRadius}
+                {padAngle}
+                {placement}
+                {...props.pie}
+                let:arcs
+              >
+                {#each arcs as arc}
+                  <Arc
+                    startAngle={arc.startAngle}
+                    endAngle={arc.endAngle}
+                    outerRadius={i * (outerRadius ?? 0)}
+                    {innerRadius}
+                    {cornerRadius}
+                    {padAngle}
+                    fill={cScale(c(arc.data))}
+                    data={arc.data}
+                    {tooltip}
+                    {...props.arc}
+                  />
+                {/each}
+              </Pie>
             {/if}
           </Group>
-        </Pie>
+        {/each}
       </slot>
 
       <slot name="above-marks" {...slotProps} />
--- packages/layerchart/src/routes/docs/components/PieChart/+page.svelte
+++ packages/layerchart/src/routes/docs/components/PieChart/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { PieChart } from 'layerchart';
+  import { group } from 'd3-array';
 
   import Preview from '$lib/docs/Preview.svelte';
-  import { createDateSeries, longData } from '$lib/utils/genData.js';
+  import { longData } from '$lib/utils/genData.js';
 
-  const data = longData.filter((d) => d.year === 2019);
+  const dataByYear = group(longData, (d) => d.year);
+  const data = dataByYear.get(2019);
 
-  // const data = createDateSeries({ min: 20, max: 100, value: 'integer', count: 4 });
-  const data2 = createDateSeries({ min: 20, max: 100, value: 'integer', count: 4 });
+  const exerciseData = [
+    { label: 'move', value: 400, total: 1000, color: '#ef4444' },
+    { label: 'exercise', value: 20, total: 30, color: '#a3e635' },
+    { label: 'stand', value: 10, total: 12, color: '#22d3ee' },
+  ];
 </script>
 
 <h1>Examples</h1>
@@ -38,30 +43,136 @@
 <h2>Arc (range)</h2>
 
 <Preview {data}>
-  <div class="h-[180px] border rounded">
+  <div class="h-[300px] border rounded">
     <PieChart
       {data}
       label="fruit"
       value="value"
       range={[-90, 90]}
-      outerRadius={256 / 2}
+      outerRadius={300 / 2}
       innerRadius={-20}
       cornerRadius={10}
       padAngle={0.02}
-      props={{ group: { y: 60 } }}
+      props={{ group: { y: 80 } }}
     />
   </div>
 </Preview>
 
-<h2>Separate tracks</h2>
+<h2>Series data</h2>
+
+<Preview data={dataByYear}>
+  <div class="h-[300px] p-4 border rounded">
+    <PieChart
+      label="fruit"
+      value="value"
+      series={Array.from(dataByYear).map(([key, data]) => {
+        return { key, data };
+      })}
+      outerRadius={-25}
+      innerRadius={-20}
+      cornerRadius={5}
+      padAngle={0.01}
+    />
+  </div>
+</Preview>
+
+<h2>Series data (individual tracks)</h2>
 
 <Preview {data}>
   <div class="h-[300px] p-4 border rounded">
     <PieChart
-      {data}
       label="fruit"
       value="value"
-      separateTracks
+      series={data?.map((d) => {
+        return {
+          key: d.fruit,
+          data: d,
+        };
+      })}
+      outerRadius={-25}
+      innerRadius={-20}
+      cornerRadius={10}
+    />
+  </div>
+</Preview>
+
+<h2>Series data (arc)</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded">
+    <PieChart
+      label="fruit"
+      value="value"
+      series={data?.map((d) => {
+        return {
+          key: d.fruit,
+          data: d,
+        };
+      })}
+      range={[-90, 90]}
+      outerRadius={-25}
+      innerRadius={-20}
+      cornerRadius={10}
+      props={{ group: { y: 190 } }}
+    />
+  </div>
+</Preview>
+
+<h2>Series data (track color)</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded">
+    <PieChart
+      label="fruit"
+      value="value"
+      series={data?.map((d) => {
+        return {
+          key: d.fruit,
+          data: d,
+        };
+      })}
+      props={{
+        arc: {
+          track: { fill: 'hsl(var(--color-surface-content) / 10%)' },
+        },
+      }}
+      outerRadius={-25}
+      innerRadius={-20}
+      cornerRadius={10}
+    />
+  </div>
+</Preview>
+
+<h2>Series data (individual tracks and total)</h2>
+
+<Preview data={exerciseData}>
+  <div class="h-[200px] p-4 border rounded">
+    <PieChart
+      label="label"
+      value="value"
+      series={exerciseData.map((d) => {
+        return {
+          key: d.label,
+          data: d,
+          total: d.total,
+          color: d.color,
+        };
+      })}
+      outerRadius={-25}
+      innerRadius={-20}
+      cornerRadius={10}
+    />
+  </div>
+</Preview>
+
+<h2>Series data (single value)</h2>
+
+<Preview>
+  <div class="h-[300px] p-4 border rounded">
+    <PieChart
+      label="label"
+      value="value"
+      series={[{ key: 'test', data: { label: 'Test', value: 70 }, total: 100 }]}
       outerRadius={-25}
       innerRadius={-20}
       cornerRadius={10}
@@ -74,6 +185,14 @@
 <Preview {data}>
   <div class="h-[300px] p-4 border rounded">
     <PieChart {data} label="fruit" value="value" props={{ arc: { class: 'stroke-surface-100' } }} />
+  </div>
+</Preview>
+
+<h2>Customize colors</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded">
+    <PieChart {data} label="fruit" value="value" cRange={['red', 'blue', 'green', 'orange']} />
   </div>
 </Preview>
 
