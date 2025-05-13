3104b9c8bfc093749f22f6de5725759bf24bfc54
Sean Lynch <techniq35@gmail.com>
2025-05-13 16:15:40 -0400

    docs: Rename "Dot Plot" to "Duration" and add bars example along with points

---
+++ .changeset/warm-mammals-deny.md
@@ -0,0 +1,5 @@
+---
+'layerchart': patch
+---
+
+docs: Rename "Dot Plot" to "Duration" and add bars example along with points
---
+++ packages/layerchart/src/routes/docs/examples/Duration/+page.svelte
@@ -0,0 +1,234 @@
+<script lang="ts">
+  import { scaleBand, scaleTime } from 'd3-scale';
+  import { addMinutes, startOfDay } from 'date-fns';
+  import { Duration } from 'svelte-ux';
+  import { PeriodType, format } from '@layerstack/utils';
+
+  import { Axis, BarChart, Chart, Highlight, LineChart, Points, Svg, Tooltip } from 'layerchart';
+
+  import Preview from '$lib/docs/Preview.svelte';
+  import { getRandomInteger } from '$lib/utils/genData.js';
+
+  const count = 10;
+  const now = startOfDay(new Date());
+  let lastStartDate = now;
+
+  const data = Array.from({ length: count }).map((_, i) => {
+    const startDate = addMinutes(lastStartDate, getRandomInteger(0, 60));
+    const endDate = addMinutes(startDate, getRandomInteger(0, 60));
+    lastStartDate = startDate;
+    return {
+      name: `Item ${i + 1}`,
+      startDate,
+      endDate,
+    };
+  });
+
+  // TODO: Update to use better data example: https://observablehq.com/@d3/dot-plot
+</script>
+
+<h1>Examples</h1>
+
+<h2>Bars</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded-sm">
+    <BarChart
+      {data}
+      x={['startDate', 'endDate']}
+      xScale={scaleTime()}
+      y="name"
+      grid={{ x: false, y: true, bandAlign: 'between' }}
+      orientation="horizontal"
+      padding={{ left: 36, bottom: 36 }}
+      props={{
+        xAxis: {
+          format: (d) => format(d, PeriodType.TimeOnly, { custom: 'h:mm aa' }),
+        },
+      }}
+    >
+      {#snippet tooltip({ context })}
+        <Tooltip.Root {context}>
+          {#snippet children({ data })}
+            <Tooltip.Header>{data.name}</Tooltip.Header>
+            <Tooltip.List>
+              <Tooltip.Item
+                label="start"
+                value={format(data.startDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Item
+                label="end"
+                value={format(data.endDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Separator />
+              <Tooltip.Item label="duration" valueAlign="right">
+                <Duration start={data.startDate} end={data.endDate} totalUnits={2} />
+              </Tooltip.Item>
+            </Tooltip.List>
+          {/snippet}
+        </Tooltip.Root>
+      {/snippet}
+    </BarChart>
+  </div>
+</Preview>
+
+<h2>Bars - color</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded-sm">
+    <BarChart
+      {data}
+      x={['startDate', 'endDate']}
+      xScale={scaleTime()}
+      y="name"
+      c="name"
+      cRange={[
+        'var(--color-danger)',
+        'var(--color-warning)',
+        'var(--color-success)',
+        'var(--color-info)',
+      ]}
+      grid={{ x: false, y: true, bandAlign: 'between' }}
+      orientation="horizontal"
+      padding={{ left: 36, bottom: 36 }}
+      props={{
+        xAxis: {
+          format: (d) => format(d, PeriodType.TimeOnly, { custom: 'h:mm aa' }),
+        },
+      }}
+    >
+      {#snippet tooltip({ context })}
+        <Tooltip.Root {context}>
+          {#snippet children({ data })}
+            <Tooltip.Header>{data.name}</Tooltip.Header>
+            <Tooltip.List>
+              <Tooltip.Item
+                label="start"
+                value={format(data.startDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Item
+                label="end"
+                value={format(data.endDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Separator />
+              <Tooltip.Item label="duration" valueAlign="right">
+                <Duration start={data.startDate} end={data.endDate} totalUnits={2} />
+              </Tooltip.Item>
+            </Tooltip.List>
+          {/snippet}
+        </Tooltip.Root>
+      {/snippet}
+    </BarChart>
+  </div>
+</Preview>
+
+<h2>Points</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded-sm">
+    <BarChart
+      {data}
+      x={['startDate', 'endDate']}
+      xScale={scaleTime()}
+      y="name"
+      grid={{ x: false, y: true, bandAlign: 'between' }}
+      orientation="horizontal"
+      padding={{ left: 36, bottom: 36 }}
+      props={{
+        highlight: {
+          axis: 'x',
+          area: true,
+          points: true,
+        },
+        xAxis: {
+          format: (d) => format(d, PeriodType.TimeOnly, { custom: 'h:mm aa' }),
+        },
+      }}
+    >
+      {#snippet marks()}
+        <Points class="fill-primary-100 stroke-primary" links={{ class: 'stroke-primary' }} />
+      {/snippet}
+
+      {#snippet tooltip({ context })}
+        <Tooltip.Root {context}>
+          {#snippet children({ data })}
+            <Tooltip.Header>{data.name}</Tooltip.Header>
+            <Tooltip.List>
+              <Tooltip.Item
+                label="start"
+                value={format(data.startDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Item
+                label="end"
+                value={format(data.endDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Separator />
+              <Tooltip.Item label="duration" valueAlign="right">
+                <Duration start={data.startDate} end={data.endDate} totalUnits={2} />
+              </Tooltip.Item>
+            </Tooltip.List>
+          {/snippet}
+        </Tooltip.Root>
+      {/snippet}
+    </BarChart>
+  </div>
+</Preview>
+
+<h2>Points - color</h2>
+
+<Preview {data}>
+  <div class="h-[300px] p-4 border rounded-sm">
+    <BarChart
+      {data}
+      x={['startDate', 'endDate']}
+      xScale={scaleTime()}
+      y="name"
+      c="name"
+      cRange={[
+        'var(--color-danger)',
+        'var(--color-warning)',
+        'var(--color-success)',
+        'var(--color-info)',
+      ]}
+      grid={{ x: false, y: true, bandAlign: 'between' }}
+      orientation="horizontal"
+      padding={{ left: 36, bottom: 36 }}
+      props={{
+        highlight: {
+          axis: 'x',
+          area: true,
+          points: true,
+        },
+        xAxis: {
+          format: (d) => format(d, PeriodType.TimeOnly, { custom: 'h:mm aa' }),
+        },
+      }}
+    >
+      {#snippet marks()}
+        <Points links />
+      {/snippet}
+
+      {#snippet tooltip({ context })}
+        <Tooltip.Root {context}>
+          {#snippet children({ data })}
+            <Tooltip.Header>{data.name}</Tooltip.Header>
+            <Tooltip.List>
+              <Tooltip.Item
+                label="start"
+                value={format(data.startDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Item
+                label="end"
+                value={format(data.endDate, PeriodType.TimeOnly, { variant: 'short' })}
+              />
+              <Tooltip.Separator />
+              <Tooltip.Item label="duration" valueAlign="right">
+                <Duration start={data.startDate} end={data.endDate} totalUnits={2} />
+              </Tooltip.Item>
+            </Tooltip.List>
+          {/snippet}
+        </Tooltip.Root>
+      {/snippet}
+    </BarChart>
+  </div>
+</Preview>
