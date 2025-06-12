efacef63b1f1a80c2a2061b47c8c939fd652a317
Sean Lynch <techniq35@gmail.com>
2025-06-12 08:42:44 -0400

    docs(SubmarineCablesGlobe): Improve perf by disabling hit canvas while spinning

--- packages/layerchart/src/routes/docs/examples/SubmarineCablesGlobe/+page.svelte
+++ packages/layerchart/src/routes/docs/examples/SubmarineCablesGlobe/+page.svelte
@@ -8,7 +8,6 @@
 
   import {
     Chart,
-    Circle,
     GeoPath,
     GeoPoint,
     GeoVisible,
@@ -80,7 +79,7 @@
       bind:context
     >
       {#snippet children({ context })}
-        <Layer type={shared.renderContext}>
+        <Layer type={shared.renderContext} disableHitCanvas={timer.running}>
           <GeoPath
             geojson={{ type: 'Sphere' }}
             class="fill-surface-200 stroke-surface-content/20"
