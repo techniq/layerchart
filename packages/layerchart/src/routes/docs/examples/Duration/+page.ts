01916717819eb4d2c38879ce189223364d9b29ca
Sean Lynch <techniq35@gmail.com>
2025-06-25 15:52:31 -0400

    feat: Add `applyLanes()` array util to support densely packing timelines

--- packages/layerchart/src/routes/docs/examples/Duration/+page.ts
+++ packages/layerchart/src/routes/docs/examples/Duration/+page.ts
@@ -1,7 +1,22 @@
+import { csvParse, autoType } from 'd3-dsv';
 import pageSource from './+page.svelte?raw';
+import type { USEvents } from '$static/data/examples/date/us-events.js';
 
 export async function load() {
   return {
+    usEvents: await fetch('/data/examples/date/us-events.csv').then(async (r) => {
+      return csvParse<USEvents>(
+        await r.text(),
+        // @ts-expect-error
+        autoType
+      ).map((d) => {
+        return {
+          startDate: new Date(d.startYear, 0, 1),
+          endDate: new Date(d.endYear, 11, 31),
+          event: d.event,
+        };
+      });
+    }),
     meta: {
       pageSource,
       supportedContexts: ['svg', 'canvas'],
