35c544601fba17e02b93067b7e1581682e99600d
Sean Lynch <techniq35@gmail.com>
2025-08-12 14:16:31 -0400

    Add simple reproduction example while investigating https://github.com/sveltejs/svelte/issues/16548

---
+++ packages/layerchart/src/routes/docs/examples/Reproduction/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Area, Axis, Chart, Layer } from 'layerchart';
+  import { createDateSeries } from '$lib/utils/genData.js';
+
+  const dateSeriesData = createDateSeries({ count: 30, min: 50, max: 100, value: 'integer' });
+</script>
+
+{#each { length: 11 } as _}
+  <div class="h-[300px] p-4 border rounded-sm">
+    <Chart
+      data={dateSeriesData}
+      x="date"
+      y="value"
+      yDomain={[0, null]}
+      yNice
+      padding={{ left: 16, bottom: 24 }}
+    >
+      <Layer type="svg">
+        <Axis placement="left" grid rule />
+        <Axis placement="bottom" rule />
+        <Area line={{ class: 'stroke-2 stroke-primary' }} class="fill-primary/30" />
+      </Layer>
+    </Chart>
+  </div>
+{/each}
---
+++ packages/layerchart/src/routes/docs/examples/Reproduction/+page.ts
@@ -0,0 +1,10 @@
+import pageSource from './+page.svelte?raw';
+
+export async function load() {
+  return {
+    meta: {
+      pageSource,
+      supportedContexts: ['svg', 'canvas'],
+    },
+  };
+}
