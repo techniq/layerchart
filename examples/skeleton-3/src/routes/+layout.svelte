3e93294f2b87a5535c8bd8f79839411538f40dcd
Sean Lynch <techniq35@gmail.com>
2025-09-21 19:28:46 -0400

    Add examples/skeleton-3

---
+++ examples/skeleton-3/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { ModeWatcher, mode, setMode } from 'mode-watcher';
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
+
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher defaultTheme="cerberus" />
+
+<main class="p-4">
+	<div class="pb-4 text-right">
+		<Switch
+			name="mode"
+			checked={mode.current === 'dark'}
+			onCheckedChange={(e) => setMode(e.checked ? 'dark' : 'light')}
+		/>
+	</div>
+
+	{@render children?.()}
+</main>
