e98f84a76b426cf11b1c8a8fcbdf9d86e1824986
Sean Lynch <techniq35@gmail.com>
2025-10-06 10:24:38 -0400

    Setup unplugin-icons and Svelte UX

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import contentCollections from '@content-collections/vite';
+import Icons from 'unplugin-icons/vite';
 // import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), contentCollections() /*, devtoolsJson()*/],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		contentCollections(),
+		Icons({ compiler: 'svelte' }) /*, devtoolsJson()*/
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
