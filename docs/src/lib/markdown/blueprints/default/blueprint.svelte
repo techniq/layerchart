6a4729142055f0500278218e37d2e6646c2e8cb3
Sean Lynch <techniq35@gmail.com>
2025-10-08 08:34:19 -0400

    Add li, ul, and ol components to markdown blueprint

--- docs/src/lib/markdown/blueprints/default/blueprint.svelte
+++ docs/src/lib/markdown/blueprints/default/blueprint.svelte
@@ -1,5 +1,20 @@
 <script lang="ts" module>
-	export { h1, h2, h3, code, img, pre, table, tr, th, td, blockquote } from '../../components';
+	export {
+		blockquote,
+		code,
+		h1,
+		h2,
+		h3,
+		img,
+		li,
+		ol,
+		pre,
+		table,
+		tr,
+		th,
+		td,
+		ul
+	} from '../../components';
 </script>
 
 <script lang="ts">
---
+++ docs/src/lib/markdown/components/li.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cls } from '@layerstack/tailwind';
+
+	let { class: className, children, ...restProps }: HTMLAttributes<HTMLLIElement> = $props();
+</script>
+
+<li class={cls('mt-2.5 leading-7 first:mt-2', className)} {...restProps}>
+	{@render children?.()}
+</li>
