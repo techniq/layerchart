5e74108c3bbcfc243e6c215a211ec7b37b3743b7
Sean Lynch <techniq35@gmail.com>
2025-10-10 08:55:01 -0400

    cleanup

--- docs/src/routes/components/[name]/[example]/+page.svelte
+++ docs/src/routes/components/[name]/[example]/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { h3 as H3 } from '$lib/markdown/blueprints/default/blueprint.svelte';
-
 	import Example from '$lib/components/Example.svelte';
 	import { page } from '$app/state';
 
