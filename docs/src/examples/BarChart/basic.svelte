10214d0e62b4679276f59f8a9329130e81ce3f03
Sean Lynch <techniq35@gmail.com>
2025-10-14 08:11:26 -0400

    docs(BarChart): Reduce basic data points from 30 to 10

--- docs/src/examples/BarChart/basic.svelte
+++ docs/src/examples/BarChart/basic.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { createDateSeries } from '$lib/utils/data';
-  import { BarChart } from 'layerchart';
+	import { createDateSeries } from '$lib/utils/data';
+	import { BarChart } from 'layerchart';
 
-  const data = createDateSeries({ count: 30, min: 50, max: 100, value: 'integer' });
-  export { data };
+	const data = createDateSeries({ count: 10, min: 20, max: 100, value: 'integer' });
+	export { data };
 </script>
 
 <BarChart {data} x="date" y="value" height={300} />
