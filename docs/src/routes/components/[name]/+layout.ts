b32de4dcf353f2be187688d01f9919f0f009abd7
Sean Lynch <techniq35@gmail.com>
2025-10-17 08:36:49 -0400

    import order

--- docs/src/routes/components/[name]/+layout.ts
+++ docs/src/routes/components/[name]/+layout.ts
@@ -1,6 +1,6 @@
+import type { Component } from 'svelte';
 import type { Examples } from '$lib/types.js';
 import { getComponentDoc } from '$lib/markdown/utils.js';
-import type { Component } from 'svelte';
 
 export const load = async ({ params }) => {
 	const allExamples = import.meta.glob('/src/examples/**/*', {
