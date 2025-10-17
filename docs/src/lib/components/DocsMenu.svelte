89035e434888117db8dbce025f7808b1cde39cba
Sean Lynch <techniq35@gmail.com>
2025-10-17 08:00:00 -0400

    Add gaps between docs sections

--- docs/src/lib/components/DocsMenu.svelte
+++ docs/src/lib/components/DocsMenu.svelte
@@ -28,11 +28,11 @@
 		.sort(sortFunc(([section]) => sectionsOrder.indexOf(section)));
 </script>
 
-<nav class={cls('flex flex-col gap-6', className)}>
+<nav class={cls('grid gap-6', className)}>
 	<section>
 		<h2 class="mb-4 text-base font-semibold capitalize">Components</h2>
 		{#each componentsBySection as [section, components]}
-			<div>
+			<div class="mb-6">
 				<h3 class="text-surface-content/50 mb-3 text-sm font-medium capitalize">{section}</h3>
 				{#each components as component}
 					<NavItem
