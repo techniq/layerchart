9066745545b304c7bb6bf5f833e3d01856e53375
Sean Lynch <techniq35@gmail.com>
2025-10-20 08:53:38 -0400

    Add anchor links for examples

--- docs/src/lib/markdown/components/h3.svelte
+++ docs/src/lib/markdown/components/h3.svelte
@@ -2,12 +2,25 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { cls } from '@layerstack/tailwind';
 
-	let { class: className, children, ...restProps }: HTMLAttributes<HTMLHeadingElement> = $props();
+	import LucideLink from '~icons/lucide/link';
+
+	let {
+		id,
+		class: className,
+		children,
+		...restProps
+	}: HTMLAttributes<HTMLHeadingElement> = $props();
 </script>
 
 <h3
-	class={cls('mt-8 scroll-m-20 text-xl font-medium tracking-[-0.01em]', className)}
+	{id}
+	class={cls('group relative mt-8 scroll-m-20 text-xl font-medium tracking-[-0.01em]', className)}
 	{...restProps}
 >
-	{@render children?.()}
+	<LucideLink
+		class="absolute left-0 top-1 pr-2 opacity-0 group-hover:opacity-100 translate-x-0 group-hover:-translate-x-full transition-all text-surface-content/50"
+	/>
+	<a href="#{id}">
+		{@render children?.()}
+	</a>
 </h3>
