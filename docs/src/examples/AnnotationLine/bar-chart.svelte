7e46cdba6d7f58c21a9b20d8797ed5aa5443ff24
“Scott <scott@rhamy.com>
2025-10-20 19:17:21 -0400

    AnnotationLine

---
+++ docs/src/examples/AnnotationLine/bar-chart.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { AnnotationLine, BarChart } from 'layerchart';
+	import { createDateSeries } from '$lib/utils/genData.js';
+
+	const dateSeriesData = createDateSeries({
+		count: 10,
+		min: 20,
+		max: 100,
+		value: 'integer',
+		keys: ['value', 'baseline']
+	});
+
+	export { dateSeriesData as data };
+</script>
+
+<BarChart data={dateSeriesData} x="date" y="value" height={300}>
+	{#snippet aboveMarks({ context })}
+		<AnnotationLine
+			y={50}
+			label="Avg"
+			props={{
+				line: { class: '[stroke-dasharray:2,2] stroke-danger' },
+				label: { class: 'fill-danger' }
+			}}
+		/>
+	{/snippet}
+</BarChart>
