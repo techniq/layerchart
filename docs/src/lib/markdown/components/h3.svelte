1e5ef0559b4c08be8b4ebe5aa234759f65ab26c3
Sean Lynch <techniq35@gmail.com>
2025-10-20 10:01:21 -0400

    Add a base header component/element to consistent #id linking

--- docs/src/lib/markdown/components/h3.svelte
+++ docs/src/lib/markdown/components/h3.svelte
@@ -1,26 +1,8 @@
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements';
-	import { cls } from '@layerstack/tailwind';
+	import HBase from './h-base.svelte';
 
-	import LucideLink from '~icons/lucide/link';
-
-	let {
-		id,
-		class: className,
-		children,
-		...restProps
-	}: HTMLAttributes<HTMLHeadingElement> = $props();
+	let props: HTMLAttributes<HTMLHeadingElement> = $props();
 </script>
 
-<h3
-	{id}
-	class={cls('group relative mt-8 scroll-m-20 text-xl font-medium tracking-[-0.01em]', className)}
-	{...restProps}
->
-	<LucideLink
-		class="absolute left-0 top-1 pr-2 opacity-0 group-hover:opacity-100 translate-x-0 group-hover:-translate-x-full transition-all text-surface-content/50"
-	/>
-	<a href="#{id}">
-		{@render children?.()}
-	</a>
-</h3>
+<HBase tag="h3" class="text-xl font-medium tracking-[-0.01em] mt-8" {...props} />
