f4bf07bcf14bd109bfd822ef94c6a2b7f8a695b9
“Scott <scott@rhamy.com>
2025-10-22 18:26:31 -0400

    Rect Example cleanup

--- docs/src/examples/Rect/styling-using-attributes.svelte
+++ docs/src/examples/Rect/styling-using-attributes.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-  import { Axis, Chart, Rect, Layer } from 'layerchart';
-
-  const data = undefined;
-  export { data };
+	import { Axis, Chart, Rect, Layer } from 'layerchart';
 </script>
 
 <Chart
-  xDomain={[0, 100]}
-  yDomain={[0, 100]}
-  padding={{ top: 10, bottom: 20, left: 24, right: 10 }}
-  height={300}
+	xDomain={[0, 100]}
+	yDomain={[0, 100]}
+	padding={{ top: 10, bottom: 20, left: 24, right: 10 }}
+	height={300}
 >
-  <Layer>
-    <Axis placement="bottom" rule />
-    <Axis placement="left" rule />
-    <Rect x={50} y={50} width={100} height={150} />
-    <Rect x={90} y={80} width={200} height={100} fill="var(--color-primary)" />
-    <Rect
-      x={125}
-      y={40}
-      width={200}
-      height={100}
-      strokeWidth={1}
-      fill="transparent"
-      stroke="var(--color-primary)"
-    />
-  </Layer>
+	<Layer>
+		<Axis placement="bottom" rule />
+		<Axis placement="left" rule />
+		<Rect x={50} y={50} width={100} height={150} />
+		<Rect x={90} y={80} width={200} height={100} fill="var(--color-primary)" />
+		<Rect
+			x={125}
+			y={40}
+			width={200}
+			height={100}
+			strokeWidth={1}
+			fill="transparent"
+			stroke="var(--color-primary)"
+		/>
+	</Layer>
 </Chart>
