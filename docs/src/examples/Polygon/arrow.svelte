003d97e482f81ad3265dd3a3e02ef8973576ef7b
“Scott <scott@rhamy.com>
2025-10-22 19:05:44 -0400

    More Polygon

--- docs/src/examples/Polygon/arrow.svelte
+++ docs/src/examples/Polygon/arrow.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-  import { Chart, Group, Layer, Polygon } from 'layerchart';
-  import { RangeField } from 'svelte-ux';
+	import { Chart, Group, Layer, Polygon } from 'layerchart';
+	import { RangeField } from 'svelte-ux';
 
-  let cornerRadius = $state(0);
+	let cornerRadius = $state(0);
 
-  const data = undefined;
-  export { data };
+	const data = undefined;
+	export { data };
 </script>
 
 <div class="flex gap-2 mb-2">
-  <RangeField label="cornerRadius" labelPlacement="left" bind:value={cornerRadius} max={50} />
+	<RangeField label="cornerRadius" labelPlacement="left" bind:value={cornerRadius} max={50} />
 </div>
 
 <Chart height={150}>
-  {#snippet children({ context })}
-    <Layer>
-      <Group x={context.width / 2} y={context.height / 2}>
-        {@const size = 60}
-        <Polygon
-          points={[
-            { x: size, y: 0 },
-            { x: size / 4, y: -size / 2 },
-            { x: size / 4, y: -size / 4 },
-            { x: -size, y: -size / 4 },
-            { x: -size, y: size / 4 },
-            { x: size / 4, y: size / 4 },
-            { x: size / 4, y: size / 2 },
-            { x: size, y: 0 },
-          ]}
-          {cornerRadius}
-        />
-      </Group>
-    </Layer>
-  {/snippet}
+	{#snippet children({ context })}
+		<Layer>
+			<Group x={context.width / 2} y={context.height / 2}>
+				{@const size = 60}
+				<Polygon
+					points={[
+						{ x: size, y: 0 },
+						{ x: size / 4, y: -size / 2 },
+						{ x: size / 4, y: -size / 4 },
+						{ x: -size, y: -size / 4 },
+						{ x: -size, y: size / 4 },
+						{ x: size / 4, y: size / 4 },
+						{ x: size / 4, y: size / 2 },
+						{ x: size, y: 0 }
+					]}
+					{cornerRadius}
+				/>
+			</Group>
+		</Layer>
+	{/snippet}
 </Chart>
--- docs/src/examples/Polygon/cross.svelte
+++ docs/src/examples/Polygon/cross.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-  import { Chart, Group, Layer, Polygon } from 'layerchart';
-  import { RangeField } from 'svelte-ux';
+	import { Chart, Group, Layer, Polygon } from 'layerchart';
+	import { RangeField } from 'svelte-ux';
 
-  let cornerRadius = $state(0);
+	let cornerRadius = $state(0);
 
-  const data = undefined;
-  export { data };
+	const data = undefined;
+	export { data };
 </script>
 
 <div class="flex gap-2 mb-2">
-  <RangeField label="cornerRadius" labelPlacement="left" bind:value={cornerRadius} max={50} />
+	<RangeField label="cornerRadius" labelPlacement="left" bind:value={cornerRadius} max={50} />
 </div>
 
 <Chart height={150}>
-  {#snippet children({ context })}
-    <Layer>
-      <Group x={context.width / 2} y={context.height / 2}>
-        {@const size = 50}
-        <Polygon
-          points={[
-            { x: -size, y: -size / 3 },
-            { x: -size / 3, y: -size / 3 },
-            { x: -size / 3, y: -size },
-            { x: size / 3, y: -size },
-            { x: size / 3, y: -size / 3 },
-            { x: size, y: -size / 3 },
-            { x: size, y: size / 3 },
-            { x: size / 3, y: size / 3 },
-            { x: size / 3, y: size },
-            { x: -size / 3, y: size },
-            { x: -size / 3, y: size / 3 },
-            { x: -size, y: size / 3 },
-          ]}
-          {cornerRadius}
-        />
-      </Group>
-    </Layer>
-  {/snippet}
+	{#snippet children({ context })}
+		<Layer>
+			<Group x={context.width / 2} y={context.height / 2}>
+				{@const size = 50}
+				<Polygon
+					points={[
+						{ x: -size, y: -size / 3 },
+						{ x: -size / 3, y: -size / 3 },
+						{ x: -size / 3, y: -size },
+						{ x: size / 3, y: -size },
+						{ x: size / 3, y: -size / 3 },
+						{ x: size, y: -size / 3 },
+						{ x: size, y: size / 3 },
+						{ x: size / 3, y: size / 3 },
+						{ x: size / 3, y: size },
+						{ x: -size / 3, y: size },
+						{ x: -size / 3, y: size / 3 },
+						{ x: -size, y: size / 3 }
+					]}
+					{cornerRadius}
+				/>
+			</Group>
+		</Layer>
+	{/snippet}
 </Chart>
