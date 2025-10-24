2f1b1a71d6a785b69a481ae4a706513b8a436c7f
“Scott <scott@rhamy.com>
2025-10-24 12:22:25 -0400

    Sparkbar

---
+++ docs/src/content/examples/Sparkbar.md
@@ -0,0 +1,32 @@
+---
+description: Sparkbar
+section: Cartesian & Polar
+layers: ['svg', 'canvas']
+related: ['components/BarChart', 'components/Bars', 'examples/Columns', 'examples/Histogram', 'examples/Sparkline']
+---
+
+<script lang="ts">
+  import Example from '$lib/components/Example.svelte';
+</script>
+
+## Examples
+
+### Basic
+
+<Example name="basic" />
+
+### Basic within a paragraph
+
+<Example name="basic-within-a-paragraph" />
+
+### Basic negative data
+
+<Example name="basic-negative-data" />
+
+### Fixed position tooltip
+
+<Example name="fixed-position-tooltip" />
+
+### Basic within a paragraph with Tooltip and Highlight
+
+<Example name="basic-within-a-paragraph-with-tooltip-and-highlight" />
---
+++ docs/src/examples/Sparkbar/basic-within-a-paragraph.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { BarChart } from 'layerchart';
+	import { createDateSeries } from '$lib/utils/data.js';
+
+	const data = createDateSeries({
+		count: 30,
+		min: 20,
+		max: 100,
+		value: 'integer',
+		keys: ['value', 'baseline']
+	});
+
+	export { data };
+</script>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium, ligula ac sollicitudin
+	ullamcorper, leo justo pretium tellus, at gravida ex quam et orci.
+	<span class="w-[124px] h-[18px] inline-block">
+		<BarChart
+			{data}
+			x="date"
+			y="value"
+			axis={false}
+			grid={false}
+			bandPadding={0.1}
+			props={{ bars: { radius: 1, strokeWidth: 0 } }}
+		/>
+	</span> Sed ipsum justo, facilisis id tempor hendrerit, suscipit eu ipsum. Mauris ut sapien quis nibh
+	volutpat venenatis. Ut viverra justo varius sapien convallis venenatis vel faucibus urna.
+</p>
