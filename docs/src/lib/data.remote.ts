f32623fd9c4a7d6f927c2fad1095b4d935928bb0
“Scott <scott@rhamy.com>
2025-10-25 10:31:58 -0400

    GeoProjection

--- docs/src/lib/data.remote.ts
+++ docs/src/lib/data.remote.ts
@@ -218,6 +218,23 @@ export const getWorldGeojson = prerender(async () => {
 	return data;
 });
 
+export const getWorldProjectionData = prerender(async () => {
+	const { fetch } = getRequestEvent();
+	const geojson = (await fetch(
+		'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'
+	).then((r) => r.json())) as Topology<{
+		countries: GeometryCollection<{ name: string }>;
+		land: GeometryCollection;
+	}>;
+	const geojsonDetail = (await fetch(
+		'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
+	).then((r) => r.json())) as Topology<{
+		countries: GeometryCollection<{ name: string }>;
+		land: GeometryCollection;
+	}>;
+	return { geojson, geojsonDetail };
+});
+
 export const getEclipses = prerender(async () => {
 	const { fetch } = getRequestEvent();
 	const data = (await fetch('/data/examples/geo/eclipses.json').then(async (r) =>
