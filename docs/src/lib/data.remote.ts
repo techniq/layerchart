6349d5d9bc29d49e73e7a05f3b3b57072d8ced64
“Scott <scott@rhamy.com>
2025-10-25 14:38:27 -0400

    combine geo.remote in to data.remote

--- docs/src/lib/data.remote.ts
+++ docs/src/lib/data.remote.ts
@@ -390,3 +390,14 @@ export const getForceGroupDots = prerender(async () => {
 	}[];
 	return data;
 });
+
+export const getCountries = prerender(async () => {
+	const { fetch } = getRequestEvent();
+	const geojson = (await fetch(
+		'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'
+	).then((r) => r.json())) as Topology<{
+		countries: GeometryCollection<{ name: string }>;
+		land: GeometryCollection;
+	}>;
+	return geojson;
+});
