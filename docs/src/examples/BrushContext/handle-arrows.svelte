224016ce0b1b960d422c209e39f14e8955e3f02e
Sean Lynch <techniq35@gmail.com>
2025-10-27 07:30:57 -0400

    Fix icon imports

--- docs/src/examples/BrushContext/handle-arrows.svelte
+++ docs/src/examples/BrushContext/handle-arrows.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { Area, Chart, Layer } from 'layerchart';
 	import { cls } from '@layerstack/tailwind';
-	import LucideChevronLeft from '~icons/lucide/chevron-left.svelte';
-	import LucideChevronRight from '~icons/lucide/chevron-right.svelte';
+	import LucideChevronLeft from '~icons/lucide/chevron-left';
+	import LucideChevronRight from '~icons/lucide/chevron-right';
 	import { getAppleStock } from '$lib/data.remote';
 
 	const data = $derived(await getAppleStock());
