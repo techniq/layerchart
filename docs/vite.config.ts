fd310ca521d8355b01efe5e4a0043bf47db85739
Sean Lynch <techniq35@gmail.com>
2025-10-26 21:20:34 -0400

    Disable browser test (until we have tests)

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -24,21 +24,20 @@ export default defineConfig({
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
+			// {
+			// 	extends: './vite.config.ts',
+			// 	test: {
+			// 		name: 'client',
+			// 		browser: {
+			// 			enabled: true,
+			// 			provider: playwright(),
+			// 			instances: [{ browser: 'chromium' }]
+			// 		},
+			// 		include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+			// 		exclude: ['src/lib/server/**'],
+			// 		setupFiles: ['./vitest-setup-client.ts']
+			// 	}
+			// },
 			{
 				extends: './vite.config.ts',
 				test: {
