bc66437f92fe05b16aa6dbc54fdfa6627c8c9116
Sean Lynch <techniq35@gmail.com>
2025-11-02 18:01:34 -0500

    Remove AreaChartSimple (outdated issue example)

--- docs/src/examples/AreaChartSimple/single-axis-y.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { AreaChart } from 'layerchart';
-	import { createDateSeries } from '$lib/utils/data.js';
-
-	const data = createDateSeries({ count: 30, min: 50, max: 100, value: 'integer' });
-	export { data };
-</script>
-
-<AreaChart {data} x="date" y="value" axis="y" height={300} />
