e8c0205406b1c9bdbe48aa89167a7bc092484a2c
Sean Lynch <techniq35@gmail.com>
2025-11-04 16:55:05 -0500

    Set filter as sticky

--- docs/src/routes/docs/examples/+page.svelte
+++ docs/src/routes/docs/examples/+page.svelte
@@ -113,8 +113,8 @@
 	)} components
 </p>
 
-<div class="grid grid-cols-[1fr_200px] gap-3">
-	<TextField placeholder="Filter" bind:value={filterQuery} class="mb-6">
+<div class="sticky top-16 grid grid-cols-[1fr_200px] gap-3 py-2 bg-surface-200 z-1">
+	<TextField placeholder="Filter" bind:value={filterQuery} clearable>
 		{#snippet prepend()}
 			<LucideSearch class="text-surface-content/50 mr-4" />
 		{/snippet}
@@ -128,7 +128,7 @@
 <div class="grid gap-10">
 	{#each visibleExamples as { component, examples } (component)}
 		<div>
-			<H2 id={component} class="sticky top-16 bg-surface-200 -mx-4 px-4 py-2">{component}</H2>
+			<H2 id={component} class="sticky top-29 bg-surface-200 -mx-4 px-4 py-2">{component}</H2>
 			<div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
 				{#each examples as example (example.name)}
 					<ExampleLink {component} example={example.name} />
