2ce4a20ac15a0907595fdb99a7b9ed1be1cbcb4c
Sean Lynch <techniq35@gmail.com>
2025-11-06 22:03:10 -0500

    Cleanup

--- docs/src/routes/docs/[type]/[name]/examples/+page.svelte
+++ docs/src/routes/docs/[type]/[name]/examples/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-	import { Button } from 'svelte-ux';
-
-	import { page } from '$app/state';
 	import Example from '$lib/components/Example.svelte';
 	import H2 from '$lib/markdown/components/h2.svelte';
-
-	import LucideChevronLeft from '~icons/lucide/chevron-left';
 
 	let { data } = $props();
 	let { catalog } = $derived(data);
