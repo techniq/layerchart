948db6a524f492c4796d9985e74e6093b6309423
Scott Rhamy <scott@rhamy.com>
2025-11-09 09:16:06 -0500

    Grid
    - "dashed-lines" - longer w/gap to make it seeable
    - Note, blank for "tick-count-responsive"

--- docs/src/examples/Grid/dashed-lines.svelte
+++ docs/src/examples/Grid/dashed-lines.svelte
@@ -4,6 +4,6 @@
 
 <Chart xDomain={[0, 100]} yDomain={[0, 100]} height={300}>
 	<Layer>
-		<Grid y={{ style: 'stroke-dasharray: 2' }} />
+		<Grid y={{ style: 'stroke-dasharray: 6 4' }} />
 	</Layer>
 </Chart>
