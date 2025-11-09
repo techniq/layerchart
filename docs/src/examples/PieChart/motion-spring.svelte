4bb2e9fe640176b2d544782e523dbbddd0ce7df0
Scott Rhamy <scott@rhamy.com>
2025-11-09 11:15:00 -0500

    PieChart
    - "segements" - controls to top for consitency.

--- docs/src/examples/PieChart/motion-spring.svelte
+++ docs/src/examples/PieChart/motion-spring.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { PieChart } from 'layerchart';
-	import { Field, Switch } from 'svelte-ux';
+	import ShowControl from '$lib/components/ShowControl.svelte';
 	import { longData } from '$lib/utils/data';
 
 	const data = longData.filter((d) => d.year === 2019);
@@ -9,10 +9,7 @@
 	let show = $state(true);
 </script>
 
-<Field label="Show" labelPlacement="left" let:id class="absolute top-2 right-2 z-1">
-	<Switch {id} bind:checked={show} size="md" />
-</Field>
-
+<ShowControl bind:show label="Show Pie" />
 <div style:height="300px">
 	{#if show}
 		<PieChart {data} key="fruit" value="value" props={{ pie: { motion: 'spring' } }} />
