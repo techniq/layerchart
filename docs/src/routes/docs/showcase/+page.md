66d229a84acfc48bd73ae0d496b63a3cfc59ad2c
Sean Lynch <techniq35@gmail.com>
2025-11-18 11:41:15 -0500

    Setup showcase page

--- docs/src/routes/docs/showcase/+page.md
+++ docs/src/routes/docs/showcase/+page.md
@@ -1,5 +1,31 @@
-- [Github Analysis](https://github.techniq.dev) ([source](https://github.com/techniq/github-analysis))
-- [Strava Analysis](https://strava.techniq.dev) ([source](https://github.com/techniq/strava-analysis))
-- [Zipline AI](https://zipline.ai/)
+<script lang="ts">
+  import { Button, Tooltip } from 'svelte-ux';
+
+  import LucideGithub from '~icons/lucide/github';
+  import LucideSquareArrowOutUpRight from '~icons/lucide/square-arrow-out-up-right'
+
+  import { sites } from './data';
+</script>
+
+# Showcase
+
+<div class="grid grid-cols-sm gap-3">
+  {#each sites as site}
+    <div class="flex flex-col border rounded-lg px-3 py-2">
+      <a href={site.url} target="_blank" class="text-base font-medium">
+        {site.name}
+      </a>
+      {#if site.description}
+        <p class="text-sm text-surface-content/50">{site.description}</p>
+      {/if}
+      <div class="grow flex items-end justify-end gap-1">
+        {#if site.source}
+          <Button href={site.source} target="_blank" icon={LucideGithub} class="size-7 text-surface-content/50 hover:text-surface-content" />
+        {/if}
+        <Button href={site.url} target="_blank" icon={LucideSquareArrowOutUpRight} class="size-7 text-surface-content/50 hover:text-surface-content" />
+      </div>
+    </div>
+  {/each}
+</div>
 
 [More](https://github.com/techniq/layerchart/network/dependents)
---
+++ docs/src/routes/docs/showcase/data.ts
@@ -0,0 +1,129 @@
+export const sites = [
+	{
+		name: 'Github Analysis',
+		description: 'Analyze your GitHub repositories and NPM packages',
+		url: 'https://github.techniq.dev',
+		source: 'https://github.com/techniq/github-analysis'
+	},
+	{
+		name: 'Strava Analysis',
+		description: 'Analyze your Strava activities',
+		url: 'https://strava.techniq.dev',
+		source: 'https://github.com/techniq/strava-analysis'
+	},
+	{
+		name: 'Zipline AI',
+		description: 'Features, context and embeddings for real-time AI/ML',
+		url: 'https://zipline.ai/'
+	},
+	{
+		name: 'shadcn-svelte',
+		description: 'shadcn/ui, but for Svelte.',
+		url: 'https://shadcn-svelte.com/',
+		source: 'https://github.com/huntabyte/shadcn-svelte'
+	},
+	{
+		name: 'Exceptionless',
+		description:
+			'Provides real-time error reporting for your JavaScript, Node, .NET Core, ASP.NET, Web API, WebForms, WPF, Console, and MVC apps',
+		url: 'https://exceptionless.com/',
+		source: 'https://github.com/exceptionless/Exceptionless'
+	},
+	{
+		name: 'AirTrail',
+		description: 'A modern, open-source personal flight tracking system',
+		url: 'http://airtrail.johan.ohly.dk/',
+		source: 'https://github.com/johanohly/AirTrail'
+	},
+	{
+		name: 'Open Archiver',
+		description: 'An open-source platform for legally compliant email archiving.',
+		url: 'https://openarchiver.com/',
+		source: 'https://github.com/LogicLabs-OU/OpenArchiver'
+	},
+	{
+		name: 'Obot',
+		description: 'Open-source MCP Gateway and AI Platform',
+		url: 'https://obot.ai/',
+		source: 'https://github.com/obot-platform/obot'
+	},
+	{
+		name: 'Tracktor',
+		description: 'Vehicle Tracking Management System',
+		url: 'https://tracktor.bytedge.in/',
+		source: 'https://github.com/javedh-dev/tracktor'
+	},
+	{
+		name: 'bili-sync',
+		url: 'https://bili-sync.allwens.work/',
+		source: 'https://github.com/amtoaer/bili-sync'
+	},
+	{
+		name: 'PowerOutage.com',
+		description: 'Tracks, records, and aggregates power outage data across the World',
+		url: 'https://poweroutage.com/'
+	},
+	{
+		name: 'IOM UN Migration: Ukraine Regional Response',
+		description: 'Needs, Intentions, and Border Crossings',
+		url: 'https://dtm.iom.int/online-interactive-resources/ukraine-regional-response-dashboard/index.html'
+	},
+	{
+		name: 'Riven',
+		description: 'Handle your Media Library with ease!',
+		url: 'https://riven.tv/',
+		source: 'https://github.com/rivenmedia/riven-frontend'
+	},
+	{
+		name: 'OtterScale',
+		description: 'A unified platform for simplified compute, storage, and networking.',
+		url: 'https://otterscale.com/',
+		source: 'https://github.com/otterscale/otterscale'
+	},
+	{
+		name: 'LearnableMeta',
+		description: 'New way to learn Geoguessr',
+		url: 'https://learnablemeta.com/',
+		source: 'https://github.com/likeon/geometa'
+	},
+	{
+		name: 'Sky Zoo',
+		description: 'Bluesky stats',
+		url: 'https://skyzoo.blue/',
+		source: 'https://github.com/jycouet/jyc.dev'
+	},
+	{
+		name: 'Open Attribution',
+		description: 'Open source MMP for ownership of your mobile ad data',
+		url: 'https://openattribution.dev/',
+		source: 'https://github.com/OpenAttribution/open-attribution'
+	},
+	{
+		name: 'Who is my neta?',
+		description:
+			'Dashboard to explore self-reported details and parliamentary activity of MPs in the 17th Lok Sabha',
+		url: 'https://whoismyneta.com/',
+		source: 'https://github.com/thedivtagguy/whoismyneta'
+	},
+	{
+		name: 'Votes in a name',
+		description: 'Exploring namesake candidates in Indian elections',
+		url: 'https://diagramchasing.fun/2024/votes-in-a-name',
+		source: 'https://github.com/diagram-chasing/votes-in-a-name'
+	},
+	{
+		name: 'Loyola Chicago: Center for Criminal Justice',
+		description: 'The First Year of the Pretrial Fairness Act',
+		url: 'https://pfa-1yr.loyolaccj.org/'
+	},
+	{
+		name: 'ftop',
+		description: 'Comperative performance metrics for Fortnite islands',
+		url: 'https://ftop.app/'
+	},
+	{
+		name: 'GEO audit',
+		description: 'GEO / AI audit that tracks your visibility impact',
+		url: 'https://www.geoaud.it/'
+	}
+];
