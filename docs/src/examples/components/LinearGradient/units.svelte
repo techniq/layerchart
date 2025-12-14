d7f5e3121c5032a000c94cd596b08175d72cb91d
Scott Rhamy <scott@rhamy.com>
2025-12-14 13:24:45 -0500

    lineargradient-horiz-scrollbars
    Linear gradient examples with lot of bars (tailwind-colors and units) commonly get clipped horizontally on narrower screens or with expanded sidepanel. This adds horizontal scrollbars when needed.

--- docs/src/examples/components/LinearGradient/units.svelte
+++ docs/src/examples/components/LinearGradient/units.svelte
@@ -2,22 +2,24 @@
 	import { Chart, Layer, LinearGradient, Rect } from 'layerchart';
 </script>
 
-<Chart height={300}>
-	<Layer>
-		<LinearGradient class="from-green-500 to-blue-500" units="objectBoundingBox">
-			{#snippet children({ gradient })}
-				{#each { length: 6 } as _, i}
-					<Rect x={0 + i * 120} y={0} width={100} height={140} rx={8} fill={gradient} />
-				{/each}
-			{/snippet}
-		</LinearGradient>
+<div class="overflow-x-auto max-w-full">
+	<Chart height={320} width={700}>
+		<Layer>
+			<LinearGradient class="from-green-500 to-blue-500" units="objectBoundingBox">
+				{#snippet children({ gradient })}
+					{#each { length: 6 } as _, i}
+						<Rect x={0 + i * 120} y={0} width={100} height={140} rx={8} fill={gradient} />
+					{/each}
+				{/snippet}
+			</LinearGradient>
 
-		<LinearGradient class="from-green-500 to-blue-500" units="userSpaceOnUse">
-			{#snippet children({ gradient })}
-				{#each { length: 6 } as _, i}
-					<Rect x={0 + i * 120} y={160} width={100} height={140} rx={8} fill={gradient} />
-				{/each}
-			{/snippet}
-		</LinearGradient>
-	</Layer>
-</Chart>
+			<LinearGradient class="from-green-500 to-blue-500" units="userSpaceOnUse">
+				{#snippet children({ gradient })}
+					{#each { length: 6 } as _, i}
+						<Rect x={0 + i * 120} y={160} width={100} height={140} rx={8} fill={gradient} />
+					{/each}
+				{/snippet}
+			</LinearGradient>
+		</Layer>
+	</Chart>
+</div>
