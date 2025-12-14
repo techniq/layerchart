69fbed8f41c0fd0543975219c0d9dd5d01d31992
Scott Rhamy <scottrhamy@me.com>
2025-12-14 12:05:42 -0500

    utils-cls-format-string (#726)
    no further errors

---
+++ docs/src/content/utils/cls.md
@@ -0,0 +1,19 @@
+---
+description: Utility function wrapper around tailwind-merge and clsx for easy style overrides.
+category: tools
+layers: []
+related: [/docs/components/BrushContext/selection, https://github.com/dcastil/tailwind-merge, https://github.com/lukeed/clsx, https://www.layerstack.dev/docs/tailwind/utils]
+---
+
+<script lang="ts">
+  import Example from '$lib/components/Example.svelte';
+  import { cls } from '@layerstack/tailwind';
+</script>
+
+## Usage
+
+### cls()
+
+<div class={cls('grid place-items-center rounded p-2 bg-error-500', true && 'bg-success-500')}>clsx allows for conditional class additions<br />tailwind-merge dedups overlapping classes</div>
+<br />
+<Example name="cls" resize={false} showcode />
---
+++ docs/src/examples/utils/format/format.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { format } from '@layerstack/utils';
+
+	import Code from '$lib/components/Code.svelte';
+
+	const fmtSource =
+		` <script lang="ts">
+  import { format } from '@layerstack/utils';
+
+  format(1234.56, "integer");
+  format(1234.56, "decimal");
+  format(1234.56, "currency");
+  format(0.5678, "percent");
+  format(0.5678, "percentRound");
+  format(1_234_567, "metric");
+  format(new Date(), "day", { variant: "short" });
+  format(new Date(), "custom", { custom: "eee, MMMM do" });
+</` +
+		`script>
+`;
+</script>
+
+{format(1234.56, 'integer')}<br />
+{format(1234.56, 'decimal')}<br />
+{format(1234.56, 'currency')}<br />
+{format(0.5678, 'percent')}<br />
+{format(0.5678, 'percentRound')}<br />
+{format(1_234_567, 'metric')}<br />
+{format(new Date(), 'day', { variant: 'short' })}<br />
+{format(new Date(), 'custom', { custom: 'eee, MMMM do' })}<br />
+<Code source={fmtSource} language="js" copyButton={false} lang="ts" />
