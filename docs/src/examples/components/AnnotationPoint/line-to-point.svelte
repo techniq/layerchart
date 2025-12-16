7f3dd49c448a21f3479e6b761a73ba51266823a2
Scott Rhamy <scottrhamy@me.com>
2025-12-16 09:31:39 -0500

    annotationpoint-update (#731)
    * annotationpoint-update
    
    - fixed annotation tooltip on "Line-to-point"
    - fixed apple events for annotation "Line-to-point"
    - Added "Apple Stock" Title for "Line-to-point"
    - "Series-annotation" - changed color for text to stand out.
    - fixed some padding
    
    * annotationpoint-update
    
    - line-to-point back to secondary color for annotation. less apt to break on copy/paste (no color-apples likely available), agreed looks better on lightmode (previously forgot to check).
    
    * annotationpoint update
    
    remove unnecessary import
    
    * Change annotation styles from 'apples' to 'secondary'
    
    ---------
    
    Co-authored-by: Sean Lynch <techniq35@gmail.com>

--- docs/src/examples/components/AnnotationPoint/line-to-point.svelte
+++ docs/src/examples/components/AnnotationPoint/line-to-point.svelte
@@ -1,29 +1,49 @@
 <script lang="ts">
-	import { AnnotationLine, AnnotationPoint, LineChart } from 'layerchart';
-	import { format, sortFunc } from '@layerstack/utils';
+	import { AnnotationLine, AnnotationPoint, LineChart, Tooltip, Text } from 'layerchart';
+	import { format } from '@layerstack/utils';
 	import { getAppleStock } from '$lib/data.remote';
 
 	const data = await getAppleStock();
 
-	// Get a few random points to use for annotations
-	const annotations = $derived(
-		[...data]
-			.sort(() => Math.random() - 0.5)
-			.slice(0, 5)
-			.sort(sortFunc('date'))
-			.map((d, i) => ({
-				x: d.date,
-				y: d.value,
-				label: String.fromCharCode(65 + i),
-				details: `This is an annotation for ${format(d.date)}`
-			}))
-	);
+	const annotations = [
+		{
+			x: new Date('June 29, 2007'),
+			y: 121.89,
+			label: 'A',
+			details: 'iPhone (1st Gen)'
+		},
+		{
+			x: new Date('July 7, 2008'),
+			y: 175.16,
+			label: 'B',
+			details: 'iPad (1st Gen)'
+		},
+		{
+			x: new Date('January 27, 2010'),
+			y: 207.88,
+			label: 'C',
+			details: 'iPad (1st Gen)'
+		},
+		{
+			x: new Date('March 11, 2011'),
+			y: 352.47,
+			label: 'D',
+			details: 'iPad (1st Gen)'
+		},
+		{
+			x: new Date('March 7, 2012'),
+			y: 545.18,
+			label: 'E',
+			details: 'Apple TV (3rd Gen)'
+		}
+	];
 
 	export { data };
 </script>
 
 <LineChart {data} x="date" y="value" height={300} padding={{ top: 10, bottom: 20, left: 25 }}>
 	{#snippet aboveMarks({ context })}
+		<Text x={context.width / 2} y={10} textAnchor="middle" value="Apple Stock" />
 		{#each annotations as annotation}
 			<AnnotationLine
 				x={annotation.x}
@@ -34,7 +54,7 @@
 			<AnnotationPoint
 				x={annotation.x}
 				y={annotation.y}
-				r={6}
+				r={8}
 				label={annotation.label}
 				details={annotation.details}
 				props={{
@@ -43,5 +63,23 @@
 				}}
 			/>
 		{/each}
+	{/snippet}
+	{#snippet tooltip({ context })}
+		<Tooltip.Root>
+			{#snippet children({ data })}
+				{#if data.annotation}
+					<!-- Annotation -->
+					<div class="whitespace-nowrap">
+						{data.annotation.details}
+					</div>
+				{:else}
+					<!-- Normal tooltip -->
+					<Tooltip.Header>{format(context.x(data), 'daytime')}</Tooltip.Header>
+					<Tooltip.List>
+						<Tooltip.Item label="value" value={context.y(data)} />
+					</Tooltip.List>
+				{/if}
+			{/snippet}
+		</Tooltip.Root>
 	{/snippet}
 </LineChart>
