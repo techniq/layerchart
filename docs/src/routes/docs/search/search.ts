3aeab3d84293fe08e2dd882859550b5fba61af28
Scott Rhamy <scott@rhamy.com>
2025-12-24 15:58:05 -0500

    flexsearch
    - added wiring for flexsearch
    - searchIndex.ts is fake data, waiting for you to scape via tools
    - supports persistent recent searches (5) saved to localstorage using PersistedState from runed.
    - Escape closes search
    - prior Ctrl-K on windows focuses URL bar (I tried at work) rather than focusing on search, now fixed.
    - This should allow you to run with it pretty quickly.
    - sections would be nice (Components, Examples, Getting Started, etc)
    - some icons too

---
+++ docs/src/routes/docs/search/search.ts
@@ -0,0 +1,66 @@
+import FlexSearch from 'flexsearch';
+
+export type Post = {
+	content: string;
+	slug: string;
+	title: string;
+};
+
+export type Result = {
+	content: string[];
+	slug: string;
+	title: string;
+};
+
+let postsIndex: InstanceType<typeof FlexSearch.Index>;
+let posts: Post[];
+
+export function createPostsIndex(data: Post[]) {
+	postsIndex = new FlexSearch.Index({ tokenize: 'forward' });
+
+	data.forEach((post, i) => {
+		const item = `${post.title} ${post.content}`;
+		postsIndex.add(i, item);
+	});
+
+	posts = data;
+}
+
+export function searchPostsIndex(searchTerm: string) {
+	const match = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	const results = postsIndex.search(match) as number[];
+
+	return results
+		.map((index: number) => posts[index])
+		.map(({ slug, title, content }: { slug: string; title: string; content: string }) => {
+			return {
+				slug,
+				title: replaceTextWithMarker(title, match),
+				content: getMatches(content, match)
+			};
+		});
+}
+
+function replaceTextWithMarker(text: string, match: string) {
+	const regex = new RegExp(match, 'gi');
+	return text.replaceAll(regex, (match) => `<mark class="rounded">${match}</mark>`);
+}
+
+function getMatches(text: string, searchTerm: string, limit = 1) {
+	const regex = new RegExp(searchTerm, 'gi');
+	const indexes = [];
+	let matches = 0;
+	let match;
+
+	while ((match = regex.exec(text)) !== null && matches < limit) {
+		indexes.push(match.index);
+		matches++;
+	}
+
+	return indexes.map((index) => {
+		const start = index - 20;
+		const end = index + 80;
+		const excerpt = text.substring(start, end).trim();
+		return `...${replaceTextWithMarker(excerpt, searchTerm)}...`;
+	});
+}
