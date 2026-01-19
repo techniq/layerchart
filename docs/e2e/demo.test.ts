a5c3acd0806b3f50272e7be98b69f96f2fe898e7
Sean Lynch <techniq35@gmail.com>
2026-01-18 23:07:10 -0500

    Remove old playwright test setup (using vitest browser mode)

--- docs/e2e/demo.test.ts
+++
@@ -1,9 +0,0 @@
-// commented out for now
-// suspect it is from long build times (including generate scripts)
-
-// import { expect, test } from '@playwright/test';
-
-// test('home page has expected h1', async ({ page }) => {
-// 	await page.goto('/');
-// 	await expect(page.locator('h1')).toBeVisible();
-// });
