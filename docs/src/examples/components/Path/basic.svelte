3e289e59e1367f03b402b1939a14df79dc3b05ec
Sean Lynch <techniq35@gmail.com>
2026-01-25 15:35:20 -0500

    Add `Path` to top-level exports and add basic example

---
+++ docs/src/examples/components/Path/basic.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Chart, Path, Layer } from 'layerchart';
+</script>
+
+<Chart height={300}>
+	<Layer>
+		<Path pathData="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" strokeWidth={2} />
+	</Layer>
+</Chart>
