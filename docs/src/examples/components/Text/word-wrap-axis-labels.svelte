054236f72b355af6814532390400d195a4bda1b5
Scott Rhamy <scottrhamy@me.com>
2026-01-27 09:49:55 -0500

    Text trunc wrap fix (#775)

--- docs/src/examples/components/Text/word-wrap-axis-labels.svelte
+++ docs/src/examples/components/Text/word-wrap-axis-labels.svelte
@@ -4,21 +4,15 @@
 
 	const data = [
 		{
-			date: new Date('2026-01-22T05:00:00.000Z'),
 			value: 67,
-			baseline: 100,
 			label: 'This is\n1st really\nlong text'
 		},
 		{
-			date: new Date('2026-01-23T05:00:00.000Z'),
 			value: 97,
-			baseline: 20,
 			label: 'This is\n2nd really\nlong text'
 		},
 		{
-			date: new Date('2026-01-24T05:00:00.000Z'),
 			value: 61,
-			baseline: 26,
 			label: 'This is\n3rd really\nlong text'
 		}
 	];
