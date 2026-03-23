bfa0c7b0ca21834eb01336c74dd007bcc8a22732
Scott Rhamy <scott@rhamy.com>
2026-03-23 19:25:33 -0400

    Add squares to markers
    Documenation listed squares as an option, but this did not exits. I have added 'square' and 'square-stroke' as options. I have also updated line and spline examples to illustrate it.

--- docs/src/examples/components/Marker/spline.svelte
+++ docs/src/examples/components/Marker/spline.svelte
@@ -17,7 +17,7 @@
 		phase: 0
 	});
 
-	const markerTypes = ['arrow', 'triangle', 'dot', 'circle', 'circle-stroke', 'line'] as const;
+	const markerTypes = ['arrow', 'triangle', 'dot', 'circle', 'circle-stroke', 'line', 'square', 'square-stroke'] as const;
 
 	const motion = $derived(config.tweened ? 'tween' : 'none');
 	const data = $derived(
