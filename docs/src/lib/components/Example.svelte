dadd4d0e0ead0f49937afb9daf62d17cb906bb08
Scott Rhamy <scott@rhamy.com>
2026-04-11 16:30:27 -0400

    Change SVG for download

--- docs/src/lib/components/Example.svelte
+++ docs/src/lib/components/Example.svelte
@@ -22,7 +22,7 @@
 	import LucideTable from '~icons/lucide/table';
 	import LucideFilePen from '~icons/lucide/file-pen';
 	import LucideGripVertical from '~icons/lucide/grip-vertical';
-	import LucideDownload from '~icons/lucide/download';
+	import LucideImageDown from '~icons/lucide/image-down';
 
 	import { page } from '$app/state';
 	import { openInStackBlitz } from '$lib/utils/stackblitz.svelte';
@@ -309,17 +309,17 @@
 				{/if}
 
 				<Toggle let:on={open} let:toggle let:toggleOff>
-					<Button icon={LucideDownload} class="text-surface-content/70 py-1" on:click={toggle}>
+					<Button icon={LucideImageDown} class="text-surface-content/70 py-1" on:click={toggle}>
 						Download
 						<Menu {open} on:close={toggleOff} placement="bottom-start" classes={{ menu: 'p-1' }}>
 							<MenuItem
-								icon={LucideDownload}
+								icon={LucideImageDown}
 								on:click={() => downloadImage(containerEl!, { filename: name ?? component })}
 							>
 								Download as PNG
 							</MenuItem>
 							{#if settings.layer !== 'canvas'}
-								<MenuItem icon={LucideDownload} on:click={handleSvgDownload}>
+								<MenuItem icon={LucideImageDown} on:click={handleSvgDownload}>
 									Download as SVG
 								</MenuItem>
 							{/if}
