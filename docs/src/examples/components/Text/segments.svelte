2a8ec5e2c62ac808ffb055765310623e0ae5cbee
Sean Lynch <techniq35@gmail.com>
2026-04-17 09:51:25 -0400

    docs(Text/Segments): Fix Html styling

--- docs/src/examples/components/Text/segments.svelte
+++ docs/src/examples/components/Text/segments.svelte
@@ -2,20 +2,20 @@
 	import { Chart, Layer, Text } from 'layerchart';
 </script>
 
-<Chart height={80}>
+<Chart height={50}>
 	<Layer>
 		<Text
 			segments={[
-				{ value: 'Revenue', class: 'text-sm font-semibold fill-primary' },
-				{ value: ' $1,200', class: 'text-xs font-light fill-surface-content/75' }
+				{ value: 'Revenue', class: 'text-sm font-semibold text-primary' },
+				{ value: ' $1,200', class: 'text-xs font-light text-surface-content/75' }
 			]}
 			x={0}
 			y={20}
 		/>
 		<Text
 			segments={[
-				{ value: 'Growth', class: 'text-sm font-semibold fill-primary' },
-				{ value: ' +12%', class: 'text-xs font-light fill-success' }
+				{ value: 'Growth', class: 'text-sm font-semibold text-primary' },
+				{ value: ' +12%', class: 'text-xs font-light text-success' }
 			]}
 			x={0}
 			y={50}
